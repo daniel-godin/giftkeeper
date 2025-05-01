58a45f58d76b3a702e37211d1308880f1b9dd522
Daniel Godin <DanielEGodin@gmail.com>
2025-05-01 07:43:42 -0700

    Commented out storage and functions for now

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
-import { getStorage, connectStorageEmulator } from "firebase/storage";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+// import { getStorage, connectStorageEmulator } from "firebase/storage";
+// import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const functions = getFunctions(app);
+// export const storage = getStorage(app);
+// export const functions = getFunctions(app);
 
 // Check if should use emulators
 const useEmulators = import.meta.env.VITE_USE_FIREBASE_EMULATORS === 'true';
@@ -20,6 +20,6 @@ if (useEmulators) {
     console.log('Using Firebase Emulators');
     connectAuthEmulator(auth, "http://127.0.0.1:9099")
     connectFirestoreEmulator(db, '127.0.0.1', 8080)
-    connectStorageEmulator(storage, "127.0.0.1", 9199);
-    connectFunctionsEmulator(functions, 'localhost', 5001);
+    // connectStorageEmulator(storage, "127.0.0.1", 9199);
+    // connectFunctionsEmulator(functions, 'localhost', 5001);
 }
