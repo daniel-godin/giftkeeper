b3e9988f0f4b3f7846ecd65cca49f19d6040fc50
Daniel Godin <DanielEGodin@gmail.com>
2025-05-01 10:47:29 -0700

    Added Router & Routed to Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
 
 
 export function App() {
-    return(<></>)
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Basic Redirect Route */}
+                <Route path="*" element={<LandingPage />} />
 
+                {/* Public Routes */}
+                <Route path="/" element={<LandingPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
