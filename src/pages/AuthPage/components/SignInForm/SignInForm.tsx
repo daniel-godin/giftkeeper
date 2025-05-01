3b0be0a199080ba6a64f4772132abd74a9a4cb81
Daniel Godin <DanielEGodin@gmail.com>
2025-05-01 11:58:22 -0700

    SignInForm Basics set up in logic and in style

--- src/pages/AuthPage/components/SignInForm/SignInForm.tsx
+++ src/pages/AuthPage/components/SignInForm/SignInForm.tsx
@@ -1,7 +1,58 @@
+import { FormEvent, useEffect, useState } from 'react'
 import styles from './SignInForm.module.css'
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 export function SignInForm () {
+    const [formData, setFormData] = useState<SignInFormData>({email: '', password: ''});
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+
+        } catch (error) {
+            console.error('Error Signing In. Error:', error);
+        }
+    }
+
     return (
-        <>SignInForm</>
+        <form className={styles.signInForm} onSubmit={handleSubmit}>
+            <h2 className={styles.formHeader}>Sign In</h2>
+            <label className={styles.label}>Email:
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className={styles.input}
+                />
+            </label>
+            <label className={styles.label}>Password:
+                <input
+                    type='password'
+                    id='password'
+                    name='password'
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    className={styles.input}
+                />
+            </label>
+
+            <button className={styles.button}>Sign In</button>
+        </form>
     )
 }
