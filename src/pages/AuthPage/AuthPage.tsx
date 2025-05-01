cafc91f3c8690a0dc70022ff5c2da7fd37dd7385
Daniel Godin <DanielEGodin@gmail.com>
2025-05-01 11:23:07 -0700

    Added an authMode and basic structure to switch between them, as well as pulling in SignInForm and SignUpForm components

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import styles from './AuthPage.module.css'
+import { SignInForm } from './components/SignInForm/SignInForm';
+import { SignUpForm } from './components/SignUpForm/SignUpForm';
+
+type AuthMode = 'signin' | 'signup';
 
 export function AuthPage () {
+    const [authMode, setAuthMode] = useState<AuthMode>('signin');
+
     return (
         <div className={styles.authPageContainer}>
             <div className={styles.authBoxContainer}>
-                
+                <div className={styles.tabs}>
+                    {/* Sign In Button */}
+                    <button
+                        className={`${styles.tab} ${authMode === 'signin' ? styles.activeTab : ''}`}
+                        onClick={() => {setAuthMode('signin')}}
+                    >
+                        Sign In
+                    </button>
+
+                    {/* Sign Up Button */}
+                    <button
+                        className={`${styles.tab} ${authMode === 'signup' ? styles.activeTab : ''}`}
+                        onClick={() => {setAuthMode('signup')}}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+
+                <div className={styles.formContainer}>
+                    {authMode === 'signin' && (<SignInForm />)}
+
+                    {authMode === 'signup' && (<SignUpForm />)}
+                </div>
             </div>
         </div>
     )
