8d7f396dee7cdb2e23ba0ed9f847ad64e09edd14
Daniel Godin <DanielEGodin@gmail.com>
2025-05-02 07:41:03 -0700

    Added ProtectedRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { LandingPage } from "./pages/LandingPage/LandingPage";
 import { AuthPage } from "./pages/AuthPage/AuthPage";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
+import { useAuth } from "./contexts/AuthContext";
 
 
 export function App() {
@@ -16,9 +17,20 @@ export function App() {
                 <Route path="/auth" element={<AuthPage />} />
 
                 {/* Protected Routes */}
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
                 
             </Routes>
         </BrowserRouter>
     )
+}
+
+// Protected Route Provides Loading & Route Protecting/Redirection (If No User Signed In)
+function ProtectedRoute ({ children } : { children: React.ReactNode }) {
+    const { authState } = useAuth();
+
+    if (authState.loading) { return <div>Loading...</div> };
+
+    if (!authState.user) { return <Navigate to='/auth' replace /> };
+
+    return <>{children}</>
 }
