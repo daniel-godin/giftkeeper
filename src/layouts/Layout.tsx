91b5fa8b7f4ea287fcff40f00c5cbde0b698c2bb
Daniel Godin <DanielEGodin@gmail.com>
2025-05-02 16:57:59 -0700

    import NavPanel component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router'
 import styles from './Layout.module.css'
+import { NavPanel } from './components/NavPanel/NavPanel'
 
 export function Layout () {
     return (
         <div className={styles.layout}>
-            <div className={styles.navContainer}></div>
+            <div className={styles.navContainer}><NavPanel /></div>
             <div className={styles.topHeaderContainer}></div>
             <div className={styles.mainContentContainer}>
                 <Outlet />
