b3ca8ba74cf2aee9ca23bd0fabfd9579d1270299
Daniel Godin <DanielEGodin@gmail.com>
2025-05-07 12:07:08 -0700

    Set up basic grid structure

--- src/layouts/components/TopBar/TopBar.tsx
+++ src/layouts/components/TopBar/TopBar.tsx
@@ -1,8 +1,32 @@
 import styles from './TopBar.module.css'
 
-export function TopBar () {
+interface TopBarProps {
+    deviceType: 'mobile' | 'desktop';
+} 
+
+export function TopBar({ deviceType }: TopBarProps) {
 
     return (
-        <>TopBar</>
+        <header className={styles.topBar}>
+            <div className={styles.logo}>
+                GiftKeeper
+            </div>
+
+            <div className={styles.middleContainer}>
+                {/* Navigation -- Desktop Only */}
+                {deviceType === 'desktop' && (
+                    <nav className={styles.desktopNavigation}>
+                        <ul className={styles.navList}>
+                            <li className={styles.navItem}>Lists</li>
+                            <li className={styles.navItem}>People</li>
+                        </ul>
+                    </nav>
+                )}  
+            </div>
+
+            <div className={styles.profileIcon}>
+                P
+            </div>
+        </header>
     )
 }
