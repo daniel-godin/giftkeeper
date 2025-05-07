17bf55f57de49bfeda27ec6e519106840fbf9a02
Daniel Godin <DanielEGodin@gmail.com>
2025-05-07 07:39:02 -0700

    Removed old NavPanel, creating a new layout.

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router'
 import styles from './Layout.module.css'
-import { NavPanel } from './components/NavPanel/NavPanel'
 
 export function Layout () {
     return (
-        <div className={styles.layout}>
-            <div className={styles.navContainer}><NavPanel /></div>
-            <div className={styles.topHeaderContainer}></div>
-            <div className={styles.mainContentContainer}>
+        <div className={styles.layoutContainer}>
+            
+
+            <main className={styles.mainContent}>
                 <Outlet />
-            </div>
+            </main>
+
+
+            
         </div>
     )
 }
