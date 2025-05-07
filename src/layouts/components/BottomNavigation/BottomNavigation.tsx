6d4de7e6a6a2348ef0a5917422e3e5ab101e3813
Daniel Godin <DanielEGodin@gmail.com>
2025-05-07 12:25:13 -0700

    Very basic link structure set up.  Needs styling fixes, possibly remove text and increase icon size

--- src/layouts/components/BottomNavigation/BottomNavigation.tsx
+++ src/layouts/components/BottomNavigation/BottomNavigation.tsx
@@ -1,8 +1,40 @@
+import { Link } from 'react-router'
 import styles from './BottomNavigation.module.css'
+import { CalendarFold, Gift, ListTodo, UsersRound } from 'lucide-react'
 
 export function BottomNavigation () {
 
     return (
-        <>BottomNavigation</>
+        <nav className={styles.bottomNav}>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>
+                    <Link to='/people' className={styles.navLink}>
+                        <span className={styles.icon}><UsersRound /></span>
+                        <span className={styles.linkLabel}>People</span>
+                    </Link>
+                </li>
+
+                <li className={styles.navItem}>
+                    <Link to='/events' className={styles.navLink}>
+                        <span className={styles.icon}><CalendarFold /></span>
+                        <span className={styles.linkLabel}>Events</span>
+                    </Link>
+                </li>
+
+                <li className={styles.navItem}>
+                    <Link to='/gift-lists' className={styles.navLink}>
+                        <span className={styles.icon}><Gift /></span>
+                        <span className={styles.linkLabel}>Gift Lists</span>
+                    </Link>
+                </li>
+
+                <li className={styles.navItem}>
+                    <Link to='/wish-lists' className={styles.navLink}>
+                        <span className={styles.icon}><ListTodo /></span>
+                        <span className={styles.linkLabel}>Wish Lists</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
