3de014da4ce47b98ff155235951202f659d0be8b
Daniel Godin <DanielEGodin@gmail.com>
2025-05-07 12:06:55 -0700

    basic grid structure

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,21 @@
 import { Outlet } from 'react-router'
 import styles from './Layout.module.css'
+import { useViewport } from '../contexts/ViewportContext'
+import { BottomNavigation } from './components/BottomNavigation/BottomNavigation';
+import { TopBar } from './components/TopBar/TopBar';
 
 export function Layout () {
+    const deviceType = useViewport();
+
     return (
         <div className={styles.layoutContainer}>
+            <TopBar deviceType={deviceType} />
             
-
             <main className={styles.mainContent}>
                 <Outlet />
             </main>
 
-
-            
+            {deviceType === 'mobile' && <BottomNavigation />}
         </div>
     )
 }
