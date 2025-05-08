f7f0d2572b0d3cd5a26fd6d3cbcb0005782fe32f
Daniel Godin <DanielEGodin@gmail.com>
2025-05-08 09:29:10 -0700

    Started Profile Page. Added a signOut button. Move the function to AuthContext later

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,26 @@
+import { signOut } from 'firebase/auth';
 import styles from './ProfilePage.module.css'
+import { auth } from '../../firebase/firebase';
 
 export function ProfilePage () {
 
+    const handleSignOut = async () => {
+        console.log('handleSignOut Triggered');
+
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.error('Error in handleSignOut:', error);
+        }
+    }
+
     return (
-        <>ProfilePage</>
+        <section className={styles.profilePage}>
+            <button onClick={handleSignOut}>Sign Out</button>
+
+
+
+
+        </section>
     )
 }
