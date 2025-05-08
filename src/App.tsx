990c3f131a6e6df78bc10930e69ab4030f8fffcc
Daniel Godin <DanielEGodin@gmail.com>
2025-05-08 07:34:30 -0700

    Added Routes for each new page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { AuthPage } from "./pages/AuthPage/AuthPage";
 import { DashboardPage } from "./pages/DashboardPage/DashboardPage";
 import { useAuth } from "./contexts/AuthContext";
 import { Layout } from "./layouts/Layout";
+import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
+import { PeoplePage } from "./pages/PeoplePage/PeoplePage";
+import { EventsPage } from "./pages/EventsPage/EventsPage";
+import { GiftListsPage } from "./pages/GiftListsPage/GiftListsPage";
+import { WishListsPage } from "./pages/WishListsPage/WishListsPage";
 
 
 export function App() {
@@ -20,9 +25,12 @@ export function App() {
                 {/* Protected Routes */}
                 <Route path="/" element={<ProtectedRoute />}>
                     <Route path="dashboard" element={<DashboardPage />} />
-
+                    <Route path="profile" element={<ProfilePage />} />
+                    <Route path="people" element={<PeoplePage />} />
+                    <Route path="events" element={<EventsPage />} />
+                    <Route path="gift-lists" element={<GiftListsPage />} />
+                    <Route path="wish-lists" element={<WishListsPage />} />
                 </Route>
-                
             </Routes>
         </BrowserRouter>
     )
