7901fddbda8d343784e54afe1cd355dcd2825deb
Daniel Godin <DanielEGodin@gmail.com>
2025-05-09 07:12:28 -0700

    Basic Profile Page scaffolding created.  Has a sign out button now, but needs reset functionsn for displayName, password, etc.

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,10 @@
 import { signOut } from 'firebase/auth';
 import styles from './ProfilePage.module.css'
 import { auth } from '../../firebase/firebase';
+import { useAuth } from '../../contexts/AuthContext';
 
 export function ProfilePage () {
+    const { authState } = useAuth();
 
     const handleSignOut = async () => {
         console.log('handleSignOut Triggered');
@@ -16,6 +18,26 @@ export function ProfilePage () {
 
     return (
         <section className={styles.profilePage}>
+            <h1>Your Profile & Settings</h1>
+
+            <div className={styles.profileInfo}>
+                <div className={styles.avatarContainer}>
+
+                </div>
+
+                <dl className={styles.infoList}>
+                    <dt>Email</dt>
+                    <dd>{authState.user?.email}</dd>
+
+                    <dt>Display Name</dt>
+                    <dd>{authState.user?.displayName}</dd>
+
+                    <dt>Account Created</dt>
+                    <dd>{authState.user?.metadata.creationTime}</dd>
+                </dl>
+            </div>
+
+
             <button onClick={handleSignOut}>Sign Out</button>
 
 
