f50405d208bbd59e736405750f1cb13ccec40436
Daniel Godin <DanielEGodin@gmail.com>
2025-05-10 11:44:10 -0700

    Created a new person works.  Using client side SDK, and Firestore rules

--- src/pages/PeoplePage/PeoplePage.tsx
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -1,8 +1,71 @@
+import { FormEvent, useState } from 'react'
 import styles from './PeoplePage.module.css'
+import { addDoc, collection, doc, serverTimestamp, setDoc, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
+import { useAuth } from '../../contexts/AuthContext';
+import { Person } from '../../types/PersonType';
 
 export function PeoplePage () {
+    const { authState } = useAuth();
+
+    const [newPerson, setNewPerson] = useState<string>('');
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // const { name, value } = e.target;
+        setNewPerson(e.target.value);
+    }
+
+    const handleNewPersonForm = async (e: FormEvent) => {
+        e.preventDefault();
+        
+        // Guard Clause.  If empty, return;  Add error message later when you create the modal.
+        if (!newPerson.trim()) { return; };
+
+        if (!authState.user) { return; };
+
+        setIsSubmitting(true);
+        try {
+            const newDocRef = doc(collection(db, 'users', authState.user.uid, 'people'));
+            const personObject: Person = {
+                id: newDocRef.id,
+                name: newPerson.trim(),
+                createdAt: serverTimestamp(),
+                updatedAt: serverTimestamp()
+            }
+
+            await setDoc(newDocRef, personObject);
+
+            console.log(`Person (${personObject.name}) created with ID: ${newDocRef.id}`)
+
+            setNewPerson('');
+            
+
+        } catch (error) {
+            console.error('Error creating new person. Error:', error);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+
 
     return (
-        <>PeoplePage</>
+        <section className={styles.peoplePage}>
+            <h1>People</h1>
+
+            <form className={styles.formCreatePerson} onSubmit={handleNewPersonForm}>
+                <input
+                    type='text'
+                    id='newPerson'
+                    name='newPerson'
+                    value={newPerson}
+                    onChange={handleInputChange}
+                    className={styles.textInput}
+                />
+                <button className={styles.submitButton}>Create New Person</button>
+
+            </form>
+
+        </section>
     )
 }
