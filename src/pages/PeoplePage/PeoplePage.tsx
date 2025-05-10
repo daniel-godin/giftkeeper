94158fe440594ac506d6a22d918f1c4058dc0979
Daniel Godin <DanielEGodin@gmail.com>
2025-05-10 15:03:50 -0700

    Cleanup & isSubmitting message in submission button

--- src/pages/PeoplePage/PeoplePage.tsx
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 import styles from './PeoplePage.module.css'
-import { addDoc, collection, doc, serverTimestamp, setDoc, Timestamp } from 'firebase/firestore';
+import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
 import { useAuth } from '../../contexts/AuthContext';
 import { Person } from '../../types/PersonType';
@@ -12,16 +12,14 @@ export function PeoplePage () {
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        // const { name, value } = e.target;
         setNewPerson(e.target.value);
     }
 
     const handleNewPersonForm = async (e: FormEvent) => {
         e.preventDefault();
         
-        // Guard Clause.  If empty, return;  Add error message later when you create the modal.
+        // Guard Clauses.  If empty, return;  Add error message later when you create the modal.
         if (!newPerson.trim()) { return; };
-
         if (!authState.user) { return; };
 
         setIsSubmitting(true);
@@ -37,10 +35,7 @@ export function PeoplePage () {
             await setDoc(newDocRef, personObject);
 
             console.log(`Person (${personObject.name}) created with ID: ${newDocRef.id}`)
-
             setNewPerson('');
-            
-
         } catch (error) {
             console.error('Error creating new person. Error:', error);
         } finally {
@@ -62,8 +57,7 @@ export function PeoplePage () {
                     onChange={handleInputChange}
                     className={styles.textInput}
                 />
-                <button className={styles.submitButton}>Create New Person</button>
-
+                <button className={styles.submitButton}>{isSubmitting ? (<>Creating New Person...</>) : (<>Create New Person</>)}</button>
             </form>
 
         </section>
