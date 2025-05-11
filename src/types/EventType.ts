7bf486652b7b423e68b2578e0d0a5b4728acf9f0
Daniel Godin <DanielEGodin@gmail.com>
2025-05-11 11:53:00 -0700

    Basic Event Type for Firestore document object

---
+++ src/types/EventType.ts
@@ -0,0 +1,17 @@
+import { FieldValue, Timestamp } from "firebase/firestore";
+import { CalendarDate } from "./CommonTypes";
+
+
+export interface Event {
+    id: string;
+    title: string;
+    date: CalendarDate; // Possibly add a "DateRange" for events that last multiple days.
+    type?: string; // Birthday, Holiday, Anniversary, etc.
+    personId?: string; // If connecting to a "Person" from (db, 'users', UUID, 'people', personId);
+    recurring?: boolean; // Annual recurring events like birthdays.
+    notes?: string;
+
+    // Metadata
+    createdAt: Timestamp | FieldValue;
+    updatedAt: Timestamp | FieldValue;
+}
