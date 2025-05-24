85ef72c2f8ad68e8cd37b6d91fcf69973c50e2df
Daniel Godin <DanielEGodin@gmail.com>
2025-05-24 12:19:09 -0700

    EventPage correctly takes in the eventId param

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import styles from './EventPage.module.css'
+import { useParams } from 'react-router';
 
 export function EventPage() {
+    const { eventId } = useParams();
+
+    // Setup a Firestore listener to doc(db, 'users', {userId}, 'events', {eventId})
+
+    useEffect(() => {
+        console.log("Event ID:", eventId);
+    }, [eventId])
 
     return (
         <>EventPage</>
