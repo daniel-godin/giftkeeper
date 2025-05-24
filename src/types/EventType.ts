e914954bbbf60e9fe1636a14006356e9c6db22b1
Daniel Godin <DanielEGodin@gmail.com>
2025-05-24 11:49:21 -0700

    Removed TempEvent & made id optional.  This allows use of this interface before creating the document in Firestore.

--- src/types/EventType.ts
+++ src/types/EventType.ts
@@ -1,25 +1,13 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
-import { CalendarDate } from "./CommonTypes";
-
 
 export interface Event {
-    id: string;
+    id?: string;
     title: string;
-    date: CalendarDate; // Possibly add a "DateRange" for events that last multiple days.
+    date: string; // Possibly add a "DateRange" for events that last multiple days.
     type?: string; // Birthday, Holiday, Anniversary, etc.
     personId?: string; // If connecting to a "Person" from (db, 'users', UUID, 'people', personId);
     recurring?: boolean; // Annual recurring events like birthdays.
     notes?: string;
-
-    // Metadata
-    createdAt: Timestamp | FieldValue;
-    updatedAt: Timestamp | FieldValue;
-}
-
-export interface TempEvent {
-    id: string;
-    title: string;
-    date: string;
 
     // Metadata
     createdAt: Timestamp | FieldValue;
