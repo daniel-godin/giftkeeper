c5a18f346ba75fa39236c53dfe7b36e9846bff2a
Daniel Godin <DanielEGodin@gmail.com>
2025-05-24 09:28:18 -0700

    useParams set up.  Tabling this page for now.  Need to add modals to the other pages first.

--- src/pages/PersonPage/PersonPage.tsx
+++ src/pages/PersonPage/PersonPage.tsx
@@ -1,6 +1,15 @@
+import { useParams } from 'react-router'
 import styles from './PersonPage.module.css'
+import { useEffect } from 'react';
 
 export function PersonPage() {
+    const { personId } = useParams();
+
+    // Setup a Firestore listener to doc(db, 'users', {userId}, 'people', {personId})
+
+    useEffect(() => {
+        console.log("Person ID:", personId);
+    }, [personId])
 
     return (
         <>PersonPage</>
