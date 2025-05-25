3793b9e64c86866d83e3d0467e87642d8d50ee57
Daniel Godin <DanielEGodin@gmail.com>
2025-05-25 13:06:27 -0700

    WishListPage now set up with useParams for further development

--- src/pages/WishListPage/WishListPage.tsx
+++ src/pages/WishListPage/WishListPage.tsx
@@ -1,6 +1,15 @@
+import { useParams } from 'react-router';
 import styles from './WishListPage.module.css'
+import { useEffect } from 'react';
 
 export function WishListPage() {
+    const { wishListId } = useParams();
+
+    // Setup a Firestore listener to doc(db, 'users', {userId}, 'giftLists', {giftListId})
+
+    useEffect(() => {
+        console.log("Wish List ID:", wishListId);
+    }, [wishListId])
 
     return (
         <>WishListPage</>
