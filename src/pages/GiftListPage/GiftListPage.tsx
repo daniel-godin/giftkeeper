ccdf7201f0bf5fc6c03fda57e2e9b2d29a98f608
Daniel Godin <DanielEGodin@gmail.com>
2025-05-25 07:21:50 -0700

    Basic GiftListPage works with useParams

--- src/pages/GiftListPage/GiftListPage.tsx
+++ src/pages/GiftListPage/GiftListPage.tsx
@@ -1,6 +1,15 @@
+import { useParams } from 'react-router';
 import styles from './GiftListPage.module.css'
+import { useEffect } from 'react';
 
 export function GiftListPage() {
+    const { giftListId } = useParams();
+
+    // Setup a Firestore listener to doc(db, 'users', {userId}, 'giftLists', {giftListId})
+
+    useEffect(() => {
+        console.log("Gift List ID:", giftListId);
+    }, [giftListId])
 
     return (
         <>GiftListPage</>
