e877fc194ca17a9c5de6cc7cc6775934d7eb9c72
Daniel Godin <DanielEGodin@gmail.com>
2025-05-27 07:43:33 -0700

    Added Metadata for GiftItem (createdAt & updatedAt)

--- src/types/GiftListType.ts
+++ src/types/GiftListType.ts
@@ -10,3 +10,15 @@ export interface GiftList {
     createdAt?: Timestamp | FieldValue;
     updatedAt?: Timestamp | FieldValue;
 }
+
+export interface GiftItem {
+    id?: string;
+    giftListId?: string; // Gift List ID that this Item belongs to.
+    name: string;
+
+    // Metadata
+    createdAt?: Timestamp | FieldValue;
+    updatedAt?: Timestamp | FieldValue;
+    
+    // This is v1.  Add more later.
+}
