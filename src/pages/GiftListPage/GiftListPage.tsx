bd3bddab713142a7f5ce5644e8de9b956d8668d2
Daniel Godin <DanielEGodin@gmail.com>
2025-05-28 07:34:10 -0700

    handleItemSave now works onBlur & onKeydown Enter. Saves to Firestore and Firestore onSnapshot does the rest

--- src/pages/GiftListPage/GiftListPage.tsx
+++ src/pages/GiftListPage/GiftListPage.tsx
@@ -1,17 +1,254 @@
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 import styles from './GiftListPage.module.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { GiftItem, GiftList } from '../../types/GiftListType';
+import { useAuth } from '../../contexts/AuthContext';
+import { collection, doc, onSnapshot, serverTimestamp, setDoc, UpdateData, updateDoc } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
+import { formatFirestoreDate } from '../../utils';
+import { EditableTitle } from '../../components/ui/EditableTitle/EditableTitle';
+import { Trash2 } from 'lucide-react';
 
 export function GiftListPage() {
     const { giftListId } = useParams();
+    const { authState } = useAuth();
 
-    // Setup a Firestore listener to doc(db, 'users', {userId}, 'giftLists', {giftListId})
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [giftList, setGiftList] = useState<GiftList>({ title: '' });
+    const [giftItems, setGiftItems] = useState<GiftItem[]>([]);
 
+    // For adding a new item:
+    const [newItem, setNewItem] = useState<GiftItem>({ name: '' });
+    const [isNewItemOpen, setIsNewItemOpen] = useState<boolean>(false);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    // Firestore onSnapshot Document Listener (Gift List): // doc(db, 'users', {userId}, 'giftLists', {giftListId})
     useEffect(() => {
-        console.log("Gift List ID:", giftListId);
-    }, [giftListId])
+        // Might need to put a re-direct or something to make sure unauthorized access doesn't happen.
+
+        // Guard Clauses:
+        if (!authState.user) {
+            setIsLoading(false);
+            return;
+        }
+
+        if (!giftListId) { 
+            setIsLoading(false);
+            return;
+        }
+
+        setIsLoading(true);
+
+        const giftListRef = doc(db, 'users', authState.user.uid, 'giftLists', giftListId);
+        const unsubscribe = onSnapshot(giftListRef, (snapshot) => {
+            // Guard Clause
+            if (!snapshot.exists()) {
+                setIsLoading(false);
+                console.error('Gift List Not Found');
+                return;
+            }
+
+            const data = snapshot.data() as GiftList;
+            setGiftList(data);
+            setIsLoading(false);
+        }, (error) => {
+            console.error('Error fetching gift list. Error:', error);
+            setIsLoading(false);
+        })
+
+        return () => unsubscribe();
+
+
+    }, []);
+
+    // Firestore onSnapshot Collection Listener (Gift Items): collection(db, 'users', {userId}, 'giftLists', {giftListId}, 'items') // Unsure about "items" as a sub-coll name.
+    useEffect(() => {
+        // Guard Clauses:
+        if (!authState.user) {
+            setIsLoading(false);
+            return;
+        }
+
+        if (!giftListId) { 
+            setIsLoading(false);
+            return;
+        }
+
+        setIsLoading(true);
+
+        const giftListItemsCollRef = collection(db, 'users', authState.user.uid, 'giftLists', giftListId, 'items');
+        const unsubscribe = onSnapshot(giftListItemsCollRef, (snapshot) => {
+            const data:GiftItem[] = [];
+            snapshot.forEach((doc) => {
+                data.push(doc.data() as GiftItem);
+            })
+            setGiftItems(data);
+            setIsLoading(false);
+        }, (error) => {
+            console.error('Error fetching gift items. Error:', error);
+            setIsLoading(false);
+        })
+
+        return () => unsubscribe();
+    }, [])
+
+    const handleTitleSave = async (newTitle: string) => {
+        if (!authState.user) { return; }
+        if (!giftListId) { return; }
+
+        try {
+            const giftListRef = doc(db, 'users', authState.user.uid, 'giftLists', giftListId);
+            await updateDoc(giftListRef, {
+                title: newTitle,
+                updatedAt: serverTimestamp()
+            })
+        } catch (error) {
+            console.error('Error updating title. Error:', error);
+        }
+    }
+
+    // Established Item Name Change
+    const handleItemSave = async (e: React.ChangeEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>, item: GiftItem, fieldName: string) => {
+        // Keyboard Event Clause:
+        if (e.type === 'keydown' && (e as React.KeyboardEvent).key !== 'Enter') { return; } ;
+
+        // Guard Clause
+        if (!authState.user || !giftListId || !item.id) { return; };
+
+        const newValue = (e.target as HTMLInputElement).value.trim();
+
+        try {
+            const docRef = doc(db, 'users', authState.user.uid, 'giftLists', giftListId, 'items', item.id);
+            const newData: UpdateData<GiftItem> = {
+                [fieldName]: newValue,
+                updatedAt: serverTimestamp()
+            }
+
+            await updateDoc(docRef, newData)
+
+            // Remove focus after Enter
+            if (e.type === 'keydown') { (e.target as HTMLInputElement).blur(); }
+
+        } catch (error) {
+            console.error('Error saving item. Error:', error);
+        }
+    }
+
+    const handleNewItemTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewItem({
+            ...newItem,
+            name: e.target.value
+        });
+    }
+
+    // Creation of new item in GiftList (GiftItem)
+    const handleNewItemSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Guard Clauses
+        if (!authState.user) { return; };
+        if (!giftListId) { return; };
+        if (!newItem.name.trim()) { return; };
+
+        setIsSubmitting(true);
+        try {
+            const newDocRef = doc(collection(db, 'users', authState.user.uid, 'giftLists', giftListId, 'items'))
+            const newDocumentData: GiftItem = {
+                id: newDocRef.id,
+                name: newItem.name,
+                createdAt: serverTimestamp(),
+                updatedAt: serverTimestamp()
+            }
+
+            await setDoc(newDocRef, newDocumentData);
+
+            setIsNewItemOpen(false);
+
+            console.log('New document created!');
+
+        } catch (error) {
+            console.error('Error submitting new item. Error:', error);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+
+    if (isLoading) { return (
+        <section className={styles.giftListPage}>
+            Loading...
+        </section>
+    )}
 
     return (
-        <>GiftListPage</>
+        <section className={styles.giftListPage}>
+            <header className={styles.header}>
+                <div className={styles.firstRowHeader}>
+                    <Link to={`/gift-lists`} className={styles.backButton}>
+                        ← Gift Lists
+                    </Link>
+                    {giftList.createdAt && (
+                        <p>Created On: {formatFirestoreDate(giftList.createdAt, 'long')}</p>
+                    )}
+                </div>
+
+                <div className={styles.secondRowHeader}>
+                    {/* Possibly add a Ternary with a "Unknown Title" */}
+                    {giftList.title && (
+                        <EditableTitle
+                            value={giftList.title}
+                            onSave={handleTitleSave}
+                            tagName='h2'
+                        />
+                    )}
+                    {giftItems && (
+                        <p>{giftItems.length} items</p>
+                    )}
+                </div>
+            </header>
+
+            <div className={styles.itemsSection}>
+                <header className={styles.itemsSectionHeader}>
+                    <h3>Gift Ideas</h3>
+                    <button onClick={() => {setIsNewItemOpen(true)}}>+ Add Item</button>
+                </header>
+
+                <div className={styles.itemsList}>
+                    {isNewItemOpen && (
+                        <form className={styles.itemContainer} autoComplete='off' onSubmit={handleNewItemSubmit}>
+                            <input type='checkbox' className={styles.checkbox} />
+                            <input 
+                                type='text' 
+                                name='newItem'
+                                onChange={handleNewItemTextChange}
+                                value={newItem.name}
+                                className={styles.inputText} 
+                            />
+                            <button>Add Item</button>
+                        </form>
+                    )}
+
+                    {giftItems.length === 0 && (
+                        <p>Add items to get started.</p>
+                    )}
+
+                    {giftItems.length > 0 && (
+                        // Map through giftItems array and make a list
+                        giftItems.map((item) => (
+                            <div key={item.id} className={styles.itemContainer}>
+                                <input type='checkbox' className={styles.checkbox} />
+                                <input 
+                                    type='text'
+                                    defaultValue={item.name}
+                                    onBlur={(e) => handleItemSave(e, item, 'name')}
+                                    onKeyDown={(e) => handleItemSave(e, item, 'name')}
+                                    className={styles.inputText} />
+                                <Trash2 />
+                            </div>
+                        ))
+                    )}
+
+                </div>
+            </div>
+        </section>
     )
 }
