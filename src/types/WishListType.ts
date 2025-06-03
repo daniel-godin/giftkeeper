4389e40ed5d792c6de69e016495143781437e88e
Daniel Godin <DanielEGodin@gmail.com>
2025-06-03 07:25:23 -0700

    New Type of 'WishItem', basically copies GiftItem.

--- src/types/WishListType.ts
+++ src/types/WishListType.ts
@@ -1,10 +1,22 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
 
-
 export interface WishList {
     id?: string;
     title: string;
     // Add more here later.
+
+    // Metadata
+    createdAt?: Timestamp | FieldValue;
+    updatedAt?: Timestamp | FieldValue;
+}
+
+export interface WishItem {
+    id?: string;
+    wishListId?: string; // WishList ID that this Item belongs to.
+    name: string;
+
+    // Details
+    purchased?: boolean;
 
     // Metadata
     createdAt?: Timestamp | FieldValue;
