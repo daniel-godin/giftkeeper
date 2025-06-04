4102e3f8547f2696909b6370597a9e9813d13cc2
Daniel Godin <DanielEGodin@gmail.com>
2025-06-04 15:24:35 -0700

    Importing people from PeopleContext works and displays correct version of the Dashboard.  Now need to build the full thing

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 import { useAuth } from '../../contexts/AuthContext'
+import { usePeople } from '../../contexts/PeopleContext';
 import styles from './DashboardPage.module.css'
 
 export function DashboardPage () {
     const { authState } = useAuth();
+    const { people, loading: peopleLoading } = usePeople();
 
-    
+    if (peopleLoading) {
+        <div>Loading People...</div>
+    }
+
+    if (people.length === 0) {
+        <div>
+            <h1>Welcome to Gift Keeper!</h1>
+            <p>Let's start by adding the people you buy gifts for.</p>
+            <button>Add Your First Person</button>
+        </div>
+    }
+
     return (
-        <>Dashboard</>
+        <div>
+            <h1>Dashboard</h1>
+            <p>You have {people.length} people in your circle.</p>
+        </div>
     )
 }
