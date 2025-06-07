2401d83a036266e9d9f8842d5cc166c367d52589
Daniel Godin <DanielEGodin@gmail.com>
2025-06-07 07:34:55 -0700

    Dashboard basics set up with a mobile and desktop verions.  Needs data pipeline before continuing

--- src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
+++ src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
@@ -8,22 +8,22 @@ export function Dashboard () {
     return (
         <div className={styles.dashboard}>
             <div className={`${styles.upcomingEvents} ${styles.dashboardBox}`}>
-                <header>Upcoming Events</header>
+                <header className={styles.headerText}>Upcoming Events</header>
                 <div className={styles.eventsList}>
 
                 </div>
                 <button>View All Events (goes to events)</button>
             </div>
 
-            <div className={`${styles.secondContainer} ${styles.dashboardBox}`}>
+            <div className={`${styles.secondContainer}`}>
                 <div className={styles.dashboardBox}>
-                    <header>People</header>
+                    <header className={styles.headerText}>People</header>
                 </div>
                 <div className={styles.dashboardBox}>
-                    <header>Gift Lists</header>
+                    <header className={styles.headerText}>Gift Lists</header>
                 </div>
                 <div className={styles.dashboardBox}>
-                    <header>Wish Lists</header>
+                    <header className={styles.headerText}>Wish Lists</header>
                 </div>
             </div>
         </div>
