67e3ad848dbd073ba2f05ee303134f727aeba899
Daniel Godin <DanielEGodin@gmail.com>
2025-06-07 10:23:48 -0700

    Added some test importing of new data providers and contexts with some console logs.  Works so far.  Might need to memo-ize later, or use offline capabilities.

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
 import { useAuth } from '../../contexts/AuthContext'
+import { useEvents } from '../../contexts/EventsContext';
+import { useGiftLists } from '../../contexts/GiftListsProvider';
 import { usePeople } from '../../contexts/PeopleContext';
+import { useWishLists } from '../../contexts/WishListsProvider';
 import styles from './DashboardPage.module.css'
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { OnboardingForm } from './components/OnboardingForm/OnboardingForm';
 
 export function DashboardPage () {
     const { authState } = useAuth();
+
+
+    // FOR TESTING PURPOSES:
     const { people, loading: peopleLoading } = usePeople();
-    // const recentPeople = people
-    // .sort((a, b) => b.updatedAt - a.updatedAt)
-    // .slice(0, 5);
+    const { events } = useEvents();
+    const { giftLists } = useGiftLists();
+    const { wishLists } = useWishLists();
+
+    useEffect(() => {
+        console.log('people:', people);
+        console.log('events:', events);
+        console.log('giftLists:', giftLists);
+        console.log('wishLists:', wishLists);
+
+    }, [people, events, giftLists, wishLists])
+
+    
 
     return (
         <section className={styles.dashboardPage}>
