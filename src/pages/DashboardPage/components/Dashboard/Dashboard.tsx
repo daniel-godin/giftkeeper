4d15a6972c92ddeb5e1b77e2364cac44bf502146
Daniel Godin <DanielEGodin@gmail.com>
2025-06-08 07:29:54 -0700

    Added data contexts, header icons, and better header text size/weight

--- src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
+++ src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,22 @@
+import { CalendarFold, Gift, ListTodo, UsersRound } from 'lucide-react';
 import { useAuth } from '../../../../contexts/AuthContext';
+import { useEvents } from '../../../../contexts/EventsContext';
+import { useGiftLists } from '../../../../contexts/GiftListsProvider';
+import { usePeople } from '../../../../contexts/PeopleContext';
+import { useWishLists } from '../../../../contexts/WishListsProvider';
 import styles from './Dashboard.module.css'
 
 export function Dashboard () {
     const { authState } = useAuth();
-    // Other Contexts Here.
+    const { people, loading: peopleLoading } = usePeople();
+    const { events, loading: eventsLoading } = useEvents();
+    const { giftLists, loading: giftListsLoading } = useGiftLists();
+    const { wishLists, loading: wishListsLoading  } = useWishLists();
 
     return (
         <div className={styles.dashboard}>
             <div className={`${styles.upcomingEvents} ${styles.dashboardBox}`}>
-                <header className={styles.headerText}>Upcoming Events</header>
+                <header className={styles.headerText}><CalendarFold /> Upcoming Events</header>
                 <div className={styles.eventsList}>
 
                 </div>
@@ -17,13 +25,13 @@ export function Dashboard () {
 
             <div className={`${styles.secondContainer}`}>
                 <div className={styles.dashboardBox}>
-                    <header className={styles.headerText}>People</header>
+                    <header className={styles.headerText}><UsersRound /> People</header>
                 </div>
                 <div className={styles.dashboardBox}>
-                    <header className={styles.headerText}>Gift Lists</header>
+                    <header className={styles.headerText}><Gift /> Gift Lists</header>
                 </div>
                 <div className={styles.dashboardBox}>
-                    <header className={styles.headerText}>Wish Lists</header>
+                    <header className={styles.headerText}><ListTodo /> Wish Lists</header>
                 </div>
             </div>
         </div>
