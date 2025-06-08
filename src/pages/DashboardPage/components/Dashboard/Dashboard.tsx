184b24490e940fe3ec531168b64d60d0928bbe83
Daniel Godin <DanielEGodin@gmail.com>
2025-06-08 07:44:57 -0700

    Loading states & display of data in each box set up.  Needs sorting and slicing next.

--- src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
+++ src/pages/DashboardPage/components/Dashboard/Dashboard.tsx
@@ -17,21 +17,74 @@ export function Dashboard () {
         <div className={styles.dashboard}>
             <div className={`${styles.upcomingEvents} ${styles.dashboardBox}`}>
                 <header className={styles.headerText}><CalendarFold /> Upcoming Events</header>
-                <div className={styles.eventsList}>
 
-                </div>
-                <button>View All Events (goes to events)</button>
+                {eventsLoading ? (
+                    <p>Loading Events...</p>
+                ) : (
+                    <>
+                        <div className={styles.eventsList}>
+                            {events.map((event) => (
+                                <p key={event.id} className={styles.item}>{event.title}</p>
+                            ))}
+                        </div>
+                        <button type='button' className={styles.linkButton}>View All Events</button>
+                    </>
+                )}
             </div>
 
             <div className={`${styles.secondContainer}`}>
                 <div className={styles.dashboardBox}>
                     <header className={styles.headerText}><UsersRound /> People</header>
+
+                    {peopleLoading ? (
+                        <p>Loading People...</p>
+                    ) : (
+                        <>
+                            <p className={styles.numberOfItems}>({people.length} people)</p>
+                            <div className={styles.recentFiveContainer}>
+                                {people.map((person) => (
+                                    <p key={person.id} className={styles.item}>{person.name}</p>
+                                ))}
+                            </div>
+                            <button type='button' className={styles.linkButton}>View All People</button>
+                        </>
+                    )}
                 </div>
+
                 <div className={styles.dashboardBox}>
                     <header className={styles.headerText}><Gift /> Gift Lists</header>
+
+                    {giftListsLoading ? (
+                        <p>Loading Gift Lists...</p>
+                    ) : (
+                        <>
+                            <p className={styles.numberOfItems}>({giftLists.length} lists)</p>
+                            <div className={styles.recentFiveContainer}>
+                                {giftLists.map((giftList) => (
+                                    <p key={giftList.id} className={styles.item}>{giftList.title}</p>
+                                ))}
+                            </div>
+                            <button type='button' className={styles.linkButton}>View All Gift Lists</button>
+                        </>
+                    )}
                 </div>
+
                 <div className={styles.dashboardBox}>
                     <header className={styles.headerText}><ListTodo /> Wish Lists</header>
+
+                    {wishListsLoading ? (
+                        <p>Loading Wish Lists...</p>
+                    ) : (
+                        <>
+                            <p className={styles.numberOfItems}>({wishLists.length} lists)</p>
+                            <div className={styles.recentFiveContainer}>
+                                {wishLists.map((wishList) => (
+                                    <p key={wishList.id} className={styles.item}>{wishList.title}</p>
+                                ))}
+                            </div>
+                            <button type='button' className={styles.linkButton}>View All Wish Lists</button>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
