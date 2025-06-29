05871543b6969f1d245d4f005ff1a35a008faf45
Daniel Godin <DanielEGodin@gmail.com>
2025-06-29 15:04:27 -0700

    imported events and upcomingEvents from data contexts.  Removed collection listener and other useState. Needs a way to show all events, currently only shows upcomingEvents

--- src/pages/EventsPage/EventsPage.tsx
+++ src/pages/EventsPage/EventsPage.tsx
@@ -1,49 +1,18 @@
-import { useEffect, useState } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
+import { useState } from 'react';
 import styles from './EventsPage.module.css'
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { db } from '../../firebase/firebase';
 import { Link } from 'react-router';
-import { Event } from '../../types/EventType';
 import { AddEventModal } from '../../components/modals/AddEventModal/AddEventModal';
+import { useEvents } from '../../contexts/EventsContext';
+import { useUpcomingEvents } from '../../hooks/useUpcomingEvents';
 
 export function EventsPage () {
-    const { authState } = useAuth();
+    // IMPORTANT NOTE:
+    // Currently *ONLY* displaying "upcomingEvents".  Need a way to display *ALL* events later.
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [events, setEvents] = useState<Event[]>([]);
+    const { events, loading: loadingEvents } = useEvents();
+    const upcomingEvents = useUpcomingEvents();
 
     const [isAddNewEventModalOpen, setIsAddNewEventModalOpen] = useState<boolean>(false);
-
-    // Firestore onSnapshot Listener for collection(db, 'users', {userId}, 'events')
-    useEffect(() => {
-        // Guard Clause
-        if (!authState.user) {
-            setEvents([]);
-            setIsLoading(false);
-            return;
-        }
-
-        setIsLoading(true);
-
-        // Firestore Queries
-        const eventsRef = collection(db, 'users', authState.user.uid, 'events');
-        const eventsQuery = query(eventsRef, orderBy('title'));
-
-        const unsubscribe = onSnapshot(eventsQuery, (snapshot) => {
-            const eventsList: Event[] = [];
-            snapshot.forEach((doc) => {
-                eventsList.push(doc.data() as Event);
-            })
-            setEvents(eventsList);
-            setIsLoading(false);
-        }, (error) => {
-            console.error('Error fetching events:', error);
-            setIsLoading(false);
-        })
-
-        return () => unsubscribe();
-    }, [])
 
     return (
         <section className={styles.eventsPage}>
@@ -52,14 +21,14 @@ export function EventsPage () {
             <button className={styles.addEventButton} onClick={() => setIsAddNewEventModalOpen(true)}>Add Event</button>
 
             <div className={styles.eventsContainer}>
-                {isLoading ? (
+                {loadingEvents ? (
                     <div className={styles.loadingMessage}>Loading events...</div>
                 ) : (
                     <div className={styles.eventsGrid}>
-                        {events.length === 0 ? (
+                        {upcomingEvents.length === 0 ? (
                             <div>No events added yet.  Create an Event to get started!</div>
                         ) : (
-                            events.map((event) => (
+                            upcomingEvents.map((event) => (
                                 <Link to={`/events/${event.id}`} key={event.id} className={styles.eventCard}>
                                     <div className={styles.eventInfo}>
                                         <div className={styles.eventTitle}>{event.title}</div>
