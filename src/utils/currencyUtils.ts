26c77e7656632feacf29bcefa406f1f4814eb38b
Daniel Godin <DanielEGodin@gmail.com>
2025-07-03 07:52:44 -0700

    New currencyUtils file.  For manipulating/calculating currency from number/cents to dollars/string

---
+++ src/utils/currencyUtils.ts
@@ -0,0 +1,12 @@
+// Utility Functions That Manipulate Currency
+// IMPORTANT:  This app stores currency in "cents" (as a Number) in database.
+
+// Convert "cents" to "dollars" -- Calculations
+export const centsToDollars = (cents: number): number => {
+    return cents / 100;
+}
+
+// For Display Purposes - Returns Formatted String With USD ($) Sign Before
+export const formatCurrency = (cents: number): string => {
+    return `$${(cents / 100).toFixed(2)}`;
+}
