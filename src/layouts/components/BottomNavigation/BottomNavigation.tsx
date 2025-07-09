d97378a7fe649b1b4ae928b1e543222695e52736
Daniel Godin <DanielEGodin@gmail.com>
2025-07-09 07:45:28 -0700

    Added plus button to open AddGiftItemModal for quick adding of gift ideas/purchases

--- src/layouts/components/BottomNavigation/BottomNavigation.tsx
+++ src/layouts/components/BottomNavigation/BottomNavigation.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router'
 import styles from './BottomNavigation.module.css'
-import { CalendarFold, Gift, ListTodo, UsersRound } from 'lucide-react'
+import { CalendarFold, Gift, ListTodo, Plus, UsersRound } from 'lucide-react'
+import { useState } from 'react'
+import { AddGiftItemModal } from '../../../components/modals/AddGiftItemModal/AddGiftItemModal';
 
 export function BottomNavigation () {
+    const [isAddGiftItemModalOpen, setIsAddGiftItemModalOpen] = useState<boolean>(false);
 
     return (
         <nav className={styles.bottomNav}>
@@ -10,31 +13,40 @@ export function BottomNavigation () {
                 <li className={styles.navItem}>
                     <Link to='/people' className={styles.navLink}>
                         <span className={styles.icon}><UsersRound /></span>
-                        {/* <span className={styles.linkLabel}>People</span> */}
                     </Link>
                 </li>
 
                 <li className={styles.navItem}>
                     <Link to='/events' className={styles.navLink}>
                         <span className={styles.icon}><CalendarFold /></span>
-                        {/* <span className={styles.linkLabel}>Events</span> */}
                     </Link>
+                </li>
+
+                <li className={styles.navItem}>
+                    <button className={`${styles.navLink} ${styles.openModalButton}`}
+                        onClick={() => setIsAddGiftItemModalOpen(true)}
+                    >
+                        <span className={styles.icon}><Plus /></span>
+                    </button>
                 </li>
 
                 <li className={styles.navItem}>
                     <Link to='/gift-lists' className={styles.navLink}>
                         <span className={styles.icon}><Gift /></span>
-                        {/* <span className={styles.linkLabel}>Gift Lists</span> */}
                     </Link>
                 </li>
 
                 <li className={styles.navItem}>
                     <Link to='/wish-lists' className={styles.navLink}>
                         <span className={styles.icon}><ListTodo /></span>
-                        {/* <span className={styles.linkLabel}>Wish Lists</span> */}
                     </Link>
                 </li>
             </ul>
+
+            <AddGiftItemModal
+                isOpen={isAddGiftItemModalOpen}
+                onClose={() => setIsAddGiftItemModalOpen(false)}
+            />
         </nav>
     )
 }
