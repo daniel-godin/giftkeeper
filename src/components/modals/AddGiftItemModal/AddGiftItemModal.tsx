de55f91475db0ef39d92ff0c569e24bb73a8e596
Daniel Godin <DanielEGodin@gmail.com>
2025-07-10 07:49:27 -0700

    Added inputs for each type of data point in Gift Item. Needs refinement and fixing of default values.

--- src/components/modals/AddGiftItemModal/AddGiftItemModal.tsx
+++ src/components/modals/AddGiftItemModal/AddGiftItemModal.tsx
@@ -7,6 +7,8 @@ import { X } from 'lucide-react';
 import { getGiftItemsCollRef } from '../../../firebase/firestore';
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { usePeople } from '../../../contexts/PeopleContext';
+import { useUpcomingEvents } from '../../../hooks/useUpcomingEvents';
+import { Event } from '../../../types/EventType';
 
 
 interface AddGiftItemModalProps {
@@ -14,23 +16,39 @@ interface AddGiftItemModalProps {
     onClose: () => void;
 }
 
+const defaultFormValues: GiftItem = {
+    id: '',
+    name: '',
+    personId: '',
+    personName: '',
+    giftListId: '',
+    status: 'idea',
+    eventId: '',
+    estimatedCost: 0,
+    purchasedCost: 0,
+}
+
 export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
     const { authState } = useAuth();
     const { people } = usePeople();
+    const upcomingEvents = useUpcomingEvents();
 
     const [status, setStatus] = useState<string>('');
-    const [formData, setFormData] = useState<GiftItem>({ name: '' });
+    const [formData, setFormData] = useState<GiftItem>(defaultFormValues);
+    const [eventOptions, setEventOptions] = useState<Event[]>([]); // Should I put the default as "upcomingEvents" would that work?
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     useEffect(() => {
         if (isOpen) {
-            setFormData({ name: '' })
+            setFormData(defaultFormValues)
             setStatus('');
-
-            // FOR TESTING:
-            console.log('people:', people);
         }
     }, [isOpen]);
+
+    // FOR TESTING PURPOSES. TODO: DELETE THIS BEFORE PUSHING TO PROD
+    useEffect(() => {
+        console.log('Form Data:', formData);
+    }, [formData])
 
     const handleTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -40,10 +58,44 @@ export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
         })
     }
 
-    const handleDropdownInputChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const handlePersonDropdownInputChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const person = people.find(person => person.id === e.target.value);
+
+        if (!person) { return }; // Guard/Optimization Clause
+        
+        setFormData({
+            ...formData,
+            personId: person.id,
+            personName: person.name,
+            giftListId: person.giftListId
+        })
+    }
+
+    const handleStatusDropdownInputChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+
+        setEventOptions(upcomingEvents);
+    }
+
+    const handleEventDropdownInputChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleCostChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
 
-        console.log('name:', name, 'value:', value);
+        setFormData({
+            ...formData,
+            [name]: Number(value.trim()) * 100, // Convert to number & multiply by 100 to get 'cents' amount.
+        })
     }
 
 
@@ -51,30 +103,30 @@ export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
         e.preventDefault();
 
         if (!authState.user) { return }; // Guard Clause
-        if (!formData.name.trim()) { return }; // Form Validation Guard Clause
+        if (!formData.name.trim() || !formData.giftListId) { return }; // Form Validation Guard Clause
 
         setIsSubmitting(true);
         setStatus('Adding New Gift Item...');
 
         try {
-            const newDocRef = doc(getGiftItemsCollRef(authState.user.uid, 'TODO-FIX-THIS-LATER-TO-DYNAMIC'));
+            const newDocRef = doc(getGiftItemsCollRef(authState.user.uid, formData.giftListId));
 
             const newDocumentData: GiftItem = {
                 id: newDocRef.id,
                 name: formData.name,
 
                 // Denormalized Data
-                // personId: string; // For connecting to Person
-                // personName?: string; // For displaying in UI, reduces unneeded lookups to find name in Person.
+                personId: formData.personId,
+                personName: formData.personName,
 
-                // // Status & Associations
-                // giftListId?: string; // Gift List ID that this Item belongs to.
-                // status?: 'idea' | 'purchased'; // idea is default.
-                // eventId?: string; // eventId that item has been 'purchased' for.
+                // Status & Associations
+                giftListId: formData.giftListId,
+                status: formData.status,
+                eventId: formData.eventId,
 
                 // // Costs -- Store in cents.  100 cents = 1 dollar.  Using 'number' for easier math.
-                // estimatedCost?: number;
-                // purchasedCost?: number;
+                estimatedCost: formData.estimatedCost,
+                purchasedCost: formData.purchasedCost,
 
                 // Metadata
                 createdAt: serverTimestamp(),
@@ -83,16 +135,16 @@ export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
 
             await setDoc(newDocRef, newDocumentData);
 
-            setStatus('New Gift List Added!! Closing in 2 seconds...');
-            setFormData({ name: '' });
+            setStatus('New Gift Item Added!! Closing in 2 seconds...');
+            setFormData(defaultFormValues);
 
             setTimeout(() => {
                 onClose();
             }, 2000);
 
         } catch (error) {
-            console.error('Error Adding New Gift List. Error:', error);
-            setStatus('Error Adding New Gift List');
+            console.error('Error Adding New Item. Error:', error);
+            setStatus('Error Adding New Gift Item. Try Again.');
         } finally {
             setIsSubmitting(false);
         }
@@ -120,9 +172,11 @@ export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
                         />
                     </label>
 
+                    {/* Selecting "person" gives personId, personName, AND ***giftListId*** (which is needed for storing Gift Item Location) */}
                     <select
                         name='person'
-                        onChange={handleDropdownInputChange}
+                        onChange={handlePersonDropdownInputChange}
+                        required={true}
                         className={styles.dropdown}
                     >
                         <option
@@ -138,6 +192,69 @@ export function AddGiftItemModal({ isOpen, onClose } : AddGiftItemModalProps) {
                             >{person.name}</option>
                         ))}
                     </select>
+
+                    {/* Changing Status will change available fields: eventId (show/hide) & estimatedCost/purchasedCost. */}
+                    <select
+                        name='status'
+                        onChange={handleStatusDropdownInputChange}
+                        required={true}
+                        className={styles.dropdown}
+                    >
+                        <option
+                            value='idea'
+                            className={styles.option}
+                        >
+                            Gift Idea
+                        </option>
+
+                        <option
+                            value='purchased'
+                            className={styles.option}
+                        >
+                            Purchased
+                        </option>
+                    </select>
+
+                    {/* Choose Event (Only if "status" === 'purchased') */}
+                    <select
+                        name='eventId'
+                        onChange={handleEventDropdownInputChange}
+                        required={false}
+                        className={styles.dropdown}
+                    >
+                        <option
+                            value=''
+                            className={styles.option}
+                        >Choose Event</option>
+
+                        {eventOptions.map(event => (
+                            <option
+                                key={event.id}
+                                value={event.id}
+                                className={styles.option}
+                            >{event.title}</option>
+                        ))}
+                    </select>
+
+                    {/* EstimatedCost or PurchasedCost (Changes depending on status === idea/purchased) */}
+                    <label>
+                        {formData.status === 'idea' && (<>Estimated Cost:</>)}
+                        {formData.status === 'purchased' && (<>Purchased Cost:</>)}
+
+                        <input
+                            type='number'
+                            name={formData.status === 'purchased' ? 'purchasedCost' : 'estimatedCost'} // 1 Input for both estimated & purchased cost
+                            step='0.01' // for 'cents'
+                            min='0'
+                            placeholder='$0.00'
+                            defaultValue={formData.status === 'purchased' ? 
+                                (formData.purchasedCost ? formData.purchasedCost / 100 : '') :
+                                (formData.estimatedCost ? formData.estimatedCost / 100 : '')
+                            }
+                            onBlur={handleCostChange}
+                            className={styles.inputText}
+                        />
+                    </label>
 
                     <output>
                         {status}
