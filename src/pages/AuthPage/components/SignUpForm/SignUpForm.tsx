f4e885503a783edfb9591f3f71f7e644a52ea864
Daniel Godin <DanielEGodin@gmail.com>
2025-07-13 12:44:22 -0700

    SignUpForm works correctly with email/password, along with a confirmation email sent.

--- src/pages/AuthPage/components/SignUpForm/SignUpForm.tsx
+++ src/pages/AuthPage/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,91 @@
+import { useNavigate } from 'react-router';
 import styles from './SignUpForm.module.css'
+import { FormEvent, useState } from 'react';
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
+import { auth } from '../../../../firebase/firebase';
+
+interface SignUpFormData {
+    email: string;
+    password: string;
+}
 
 export function SignUpForm () {
+    const navigate = useNavigate();
+
+    const [status, setStatus] = useState<string>('');
+    const [formData, setFormData] = useState<SignUpFormData>({ email: '', password: '' });
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        if (!formData.email || !formData.password) { return }; // Guard Clause
+        if (formData.password.length < 8) { 
+            setStatus('Password must be at least 8+ characters'); 
+            return 
+        };
+
+        setStatus('Signing Up...');
+        setIsSubmitting(true);
+        try {
+            const newUser = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
+            await sendEmailVerification(newUser.user);
+
+            setStatus('Check Email To Verify Your Email.  Redirecting you...');
+            setTimeout(() => {
+                navigate('/dashboard');
+            }, 1500)
+        } catch (error: any) {
+            let errorMessage = 'Error creating account. Please try again.';
+             
+            if (error.code === 'auth/email-already-in-use') {
+                errorMessage = 'This email is already registered';
+            } else if (error.code == 'auth/invalid-email') {
+                errorMessage = 'Please enter a valid email';
+            }
+
+            setStatus(errorMessage);
+            setIsSubmitting(false);
+        }
+    }
+
     return (
-        <>SignUpForm</>
+        <form className={styles.signUpForm} onSubmit={handleSubmit}>
+            <h2 className={styles.formHeader}>Sign Up</h2>
+            <label className={styles.label}>Email:
+                <input
+                    type='email'
+                    name='email'
+                    value={formData.email}
+                    disabled={isSubmitting}
+                    onChange={handleInputChange}
+                    className={styles.input}
+                />
+            </label>
+            <label className={styles.label}>Password:
+                <input
+                    type='password'
+                    name='password'
+                    minLength={8}
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    disabled={isSubmitting}
+                    className={styles.input}
+                />
+            </label>
+
+            <button type='submit' className={styles.button} disabled={isSubmitting}>Sign Up</button>
+
+            {status && (<p className={styles.statusText}>{status}</p>)}
+        </form>
     )
 }
