0f3fffa00f5e6e3f064ef9079b902ceae10e2f60
Daniel Godin <DanielEGodin@gmail.com>
2025-07-14 16:16:50 -0700

    Added conditional rendering for Estimated Cost v Purchased Cost text

--- src/components/GiftItemCard/GiftItemCard.tsx
+++ src/components/GiftItemCard/GiftItemCard.tsx
@@ -3,12 +3,15 @@ import { GiftItem } from '../../types/GiftListType'
 import styles from './GiftItemCard.module.css'
 import { capitalizeFirst } from '../../utils/stringUtils';
 import { formatCurrency } from '../../utils/currencyUtils';
+import { useState } from 'react';
+import { EditGiftItemModal } from '../modals/EditGiftItemModal/EditGiftItemModal';
 
 interface GiftItemCardProps {
     item: GiftItem;
 }
 
 export function GiftItemCard({ item } : GiftItemCardProps) {
+    const [isEditGiftItemModalOpen, setIsEditGiftItemModalOpen] = useState<boolean>(false);
 
     return (
         <div key={item.id} className={styles.giftItemCard}>
@@ -30,9 +33,24 @@ export function GiftItemCard({ item } : GiftItemCardProps) {
                 )}
             </div>
             <div className={styles.giftItemCardRow}>
-                <span className={styles.giftItemCategory}>Cost</span>
+                <span className={styles.giftItemCategory}>
+                    {item.status === 'idea' && (<>Estimated Cost</>)}
+                    {item.status === 'purchased' && (<>Purchased Cost</>)}
+                </span>
                 <span className={styles.giftItemDetail}>{formatCurrency(item.purchasedCost || 0)}</span>
             </div>
+            <div className={styles.giftItemCardRow}>
+                <span className={styles.giftItemCategory}></span>
+                <button type='button' className={styles.actionsButton} onClick={() => setIsEditGiftItemModalOpen(true)}>Edit Item</button>
+            </div>
+            
+            {/* Edit Modal */}
+            <EditGiftItemModal
+                isOpen={isEditGiftItemModalOpen}
+                onClose={() => setIsEditGiftItemModalOpen(false)}
+                data={item}
+            />
+
         </div>
     )
 }
