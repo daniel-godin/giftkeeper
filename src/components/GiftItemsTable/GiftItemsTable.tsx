83a316b2010119290a279473d2819ee02cad89fb
Daniel Godin <DanielEGodin@gmail.com>
2025-07-15 13:54:37 -0700

    GIftItemsTable v1 complete.  Might need some UI/UX improvements, but good enough to use throughout the website.

--- src/components/GiftItemsTable/GiftItemsTable.tsx
+++ src/components/GiftItemsTable/GiftItemsTable.tsx
@@ -1,4 +1,4 @@
-import { Check, Lightbulb } from 'lucide-react';
+import { Check, ExternalLink, Lightbulb } from 'lucide-react';
 import { GiftItem } from '../../types/GiftListType'
 import styles from './GiftItemsTable.module.css'
 import { capitalizeFirst } from '../../utils/stringUtils';
@@ -20,58 +20,75 @@ export function GiftItemsTable({ data } : GiftItemsTableProps) {
         setIsEditGiftItemModalOpen(true);
     } 
 
-    if (data.length === 0) {
-        return (<div>No Gift Items Found.</div>)
-    }
+    // Return Nothing If No Data in Data Array
+    if (data.length === 0) { return (<div>No Gift Items Found.</div>) }
 
-    if (data.length > 0) {
-        return (
+    return (
+        <>
             <table className={styles.giftItemsTable}>
                 <thead className={styles.tableHead}>
                     <tr className={`${styles.tableHeadRow} ${styles.tableRow}`}>
-                        <th scope='col'>PERSON</th>
-                        <th scope='col'>GIFT ITEM</th>
-                        <th scope='col'>STATUS</th>
-                        <th scope='col'>COST</th> 
-                        <th scope='col'>ACTION</th>
+                        <th scope='col' className={styles.tableCell}>PERSON</th>
+                        <th scope='col' className={styles.tableCell}>GIFT ITEM</th>
+                        <th scope='col' className={styles.tableCell}>LINK</th> 
+                        <th scope='col' className={styles.tableCell}>STATUS</th>
+                        <th scope='col' className={styles.tableCell}>COST</th> 
+                        <th scope='col' className={styles.tableCell}>ACTION</th>
                     </tr>
                 </thead>
                 <tbody className={styles.tableBody}>
-                    {data && (
-                        data.map(item => (
-                            <tr key={item.id}>
-                                <td>{item.personName}</td>
-                                <td>{item.name}</td>
-
-                                {/* Conditional Rendering Based on Status. idea = estimatedCost, purchased = purchasedCost */}
-                                {item.status === 'idea' ? (
-                                    <>
-                                        <td className={styles.giftItemDetailIdea}><Lightbulb size={20}/> {capitalizeFirst(item.status)}</td>
-                                        <td>{formatCurrency(item.estimatedCost || 0)}</td>
-                                    </>
-                                ) : item.status === 'purchased' && ( // Means "not" idea, only other option is "purchased".
-                                    <>
-                                        <td className={styles.giftItemDetailPurchased}><Check size={20}/> {capitalizeFirst(item.status)}</td>
-                                        <td>{formatCurrency(item.purchasedCost || 0)}</td>
-                                    </>
+                    {data.map(item => (
+                        <tr key={item.id} className={styles.tableRow}>
+                            <td className={styles.tableCell}>{item.personName}</td>
+                            <td className={styles.tableCell}>{item.name}</td>
+          
+                            {/* Link is based on item.url */}
+                            <td className={styles.tableCell}>
+                                {item.url && (
+                                    <a
+                                        href={item.url}
+                                        target='_blank'
+                                        // className='unstyled-link'
+                                        title='View Item Online'
+                                    >
+                                        <ExternalLink size={16} />
+                                    </a>
                                 )}
-                                <td>
-                                    <button type='button' className={styles.actionsButton} onClick={() => handleEditableItem(item)}>
-                                        Edit Item
-                                    </button>
-                                </td>
-                            </tr>
-                        ))
-                    )}
-                </tbody>
+                            </td>
 
-                {/* Edit Gift Item Modal */}
-                <EditGiftItemModal
-                    isOpen={isEditGiftItemModalOpen}
-                    onClose={() => setIsEditGiftItemModalOpen(false)}
-                    data={giftItemBeingEdited}
-                />
+                            {/* Conditional Rendering Based on Status. idea = estimatedCost, purchased = purchasedCost */}
+                            {item.status === 'idea' && (
+                                <>
+                                    <td className={`${styles.tableCell}`}><span className={styles.giftItemDetailIdea}><Lightbulb size={20}/> {capitalizeFirst(item.status)}</span></td>
+                                    <td className={styles.tableCell}>{formatCurrency(item.estimatedCost || 0)}</td>
+                                </>
+                            )}
+                            {/* status === purchased = purchasedCost & Different Display */}
+                            {item.status === 'purchased' && (
+                                <>
+                                    <td className={`${styles.tableCell}`}><span className={styles.giftItemDetailPurchased}><Check size={20}/> {capitalizeFirst(item.status)}</span></td>
+                                    <td className={styles.tableCell}>{formatCurrency(item.purchasedCost || 0)}</td>
+                                </>
+                            )}
+
+
+
+                            <td className={styles.tableCell}>
+                                <button type='button' className={styles.actionsButton} onClick={() => handleEditableItem(item)}>
+                                    Edit Item
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-        )
-    }
+
+            {/* Edit Gift Item Modal */}
+            <EditGiftItemModal
+                isOpen={isEditGiftItemModalOpen}
+                onClose={() => setIsEditGiftItemModalOpen(false)}
+                data={giftItemBeingEdited}
+            />
+        </>
+    )
 }
