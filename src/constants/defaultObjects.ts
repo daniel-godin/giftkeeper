0e710c16eb5e4614e38f74fa720d1e12b90f92ed
Daniel Godin <DanielEGodin@gmail.com>
2025-07-15 07:40:56 -0700

    init of constants. First constant is for DEFAULT_GIFT_ITEM Object.

---
+++ src/constants/defaultObjects.ts
@@ -0,0 +1,16 @@
+import { GiftItem } from "../types/GiftListType";
+
+
+export const DEFAULT_GIFT_ITEM: GiftItem ={
+    id: '',
+    name: '',
+    personId: '',
+    personName: '',
+    giftListId: '',
+    status: 'idea', // idea is always default. 'idea' | 'purchased'
+    eventId: '',
+    url: '',
+    estimatedCost: 0,
+    purchasedCost: 0,
+    // Shouldn't need createdAt or updatedAt.  These are handled under the hood, not "frontend" interactive.
+}
