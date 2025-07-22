5c4e2b5fa34cb5989f646f0d369646e5f674c8b1
Daniel Godin <DanielEGodin@gmail.com>
2025-07-22 11:47:34 -0700

    FormInput v1 complete. Business Logic & CSS Styling

--- src/components/ui/FormInput/FormInput.tsx
+++ src/components/ui/FormInput/FormInput.tsx
@@ -1,8 +1,62 @@
 import styles from './FormInput.module.css'
 
-export function FormInput() {
+interface FormInputProps {
+    label?: string;
+    labelClassName?: string;
 
-    return (
-        <>FormInput</>
+    type?: 'text' | 'email' | 'number' | 'date';
+    name?: string;
+    id?: string;
+    placeholder?: string;
+    required?: boolean;
+    disabled?: boolean; // For Disabling During Submission
+    value?: string | number;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    onFocus?: () => void;
+    onBlur?: () => void;
+    inputClassName?: string
+}
+
+export function FormInput({
+    label,
+    labelClassName,
+    type = 'text',
+    name,
+    id,
+    placeholder,
+    required = false,
+    disabled = false,
+    value,
+    onChange,
+    onFocus,
+    onBlur,
+    inputClassName
+} : FormInputProps) {
+
+    const inputElement = (
+        <input
+            type={type} // Default: 'text'
+            name={name}
+            id={id}
+            placeholder={placeholder}
+            required={required} // Default: false
+            disabled={disabled} // Default: false
+            value={value}
+            onChange={onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            aria-required={required}
+            className={`${styles.input} ${inputClassName || ''}`}
+        />
+    )
+
+    // If (label): Render both <label> AND <input>, otherwise, just <input>
+    return label ? (
+        <label className={`${styles.label} ${labelClassName || ''}`}>
+            {label} {required && (<span className={styles.required}>*</span>)}
+            {inputElement}
+        </label>
+    ) : (
+        inputElement
     )
 }
