ad5c0a913682ae089336febb44231dd9fbfbda35
Daniel Godin <DanielEGodin@gmail.com>
2025-07-22 07:00:15 -0700

    New centralized export for UI Components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+// Centralized export of ui components.
+
+export { EditableTitle } from './EditableTitle/EditableTitle';
+export { QuickAddButton } from './QuickAddButton/QuickAddButton';
+
+// Form Components:
