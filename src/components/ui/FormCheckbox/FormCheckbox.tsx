3f84ea4f9063e44162ec7e7dcf895be769125768
Daniel Godin <DanielEGodin@gmail.com>
2025-07-23 15:17:01 -0700

    FormCheckbox v1 complete.

--- src/components/ui/FormCheckbox/FormCheckbox.tsx
+++ src/components/ui/FormCheckbox/FormCheckbox.tsx
@@ -1,8 +1,49 @@
 import styles from './FormCheckbox.module.css'
 
-export function FormCheckbox() {
+interface FormCheckboxProps {
+    label?: string;
+    labelClassName?: string;
+    name?: string;
+    id?: string;
+    required?: boolean;
+    disabled?: boolean;
+    checked: boolean;
+    onChange: (checked: boolean) => void;
+    inputClassName?: string;
+}
 
-    return (
-        <>FormCheckbox</>
+export function FormCheckbox({
+    label,
+    labelClassName,
+    name,
+    id,
+    required = false,
+    disabled = false,
+    checked,
+    onChange,
+    inputClassName
+} : FormCheckboxProps) {
+
+    const checkboxElement = (
+        <input
+            type='checkbox'
+            name={name}
+            id={id}
+            required={required}
+            disabled={disabled}
+            checked={checked}
+            onChange={(e) => onChange(e.target.checked)}
+            aria-required={required}
+            className={`${styles.input} ${inputClassName || ''}`}
+        />
+    )
+
+    return label ? (
+        <label className={`${styles.label} ${labelClassName || ''}`}>
+            {checkboxElement}
+            {label}
+        </label>
+    ) : (
+        checkboxElement
     )
 }
