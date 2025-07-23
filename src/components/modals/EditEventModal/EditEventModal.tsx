298b47a6de2c94b26973c0b37572c8a486839c76
Daniel Godin <DanielEGodin@gmail.com>
2025-07-23 07:00:03 -0700

    Basics set up

--- src/components/modals/EditEventModal/EditEventModal.tsx
+++ src/components/modals/EditEventModal/EditEventModal.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
+import { useAuth } from '../../../contexts/AuthContext';
+import { Event } from '../../../types/EventType';
+import { BaseModal } from '../BaseModal/BaseModal'
 import styles from './EditEventModal.module.css'
 
-export function EditEventModal() {
+interface EditEventModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    eventData: Event;
+}
+
+export function EditEventModal({ isOpen, onClose, eventData } : EditEventModalProps) {
+    const { authState } = useAuth();
+
+    const [status, setStatus] = useState<string>('');
+    const [formData, setFormData] = useState<Event>(eventData);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     return (
-        <>EditEventModal</>
+        <BaseModal isOpen={isOpen} onClose={onClose}>
+            <div className={styles.editEventModal}>
+
+            </div>
+        </BaseModal>
     )
 }
