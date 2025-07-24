02312cb01a821b0d9b4eb95fb68c878b2d6bd9aa
Daniel Godin <DanielEGodin@gmail.com>
2025-07-24 07:46:54 -0700

    + Add New Person now properly submits on 'Enter', as well as auto-focuses when clicked

--- src/components/ui/FormPeopleSelector/FormPeopleSelector.tsx
+++ src/components/ui/FormPeopleSelector/FormPeopleSelector.tsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Person } from '../../../types/PersonType';
 import { FormCheckbox } from '../FormCheckbox/FormCheckbox';
 import styles from './FormPeopleSelector.module.css'
 import { FormInput } from '../FormInput/FormInput';
+import { DEFAULT_PERSON } from '../../../constants/defaultObjects';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { getPeopleCollRef } from '../../../firebase/firestore';
+import { useAuth } from '../../../contexts/AuthContext';
 
 interface FormPeopleSelectorProps {
     legendText?: string;
@@ -26,21 +30,65 @@ export function FormPeopleSelector({
     allowCreateNewPerson = false,
     onCreatePerson
 } : FormPeopleSelectorProps) {
+    const { authState } = useAuth();
+
+    const newPersonInputRef = useRef<HTMLInputElement>(null);
 
     const [isCreateNewPersonEnabled, setIsCreateNewPersonEnabled] = useState<boolean>(false);
+    const [newPerson, setNewPerson] = useState<Person>(DEFAULT_PERSON);
+    const [isCreatingNewPerson, setIsCreatingNewPerson] = useState<boolean>(false);
 
-    const [newPersonName, setNewPersonName] = useState<string>('');
-
+    useEffect(() => {
+        if (isCreateNewPersonEnabled && newPersonInputRef.current) {
+            newPersonInputRef.current.focus();
+        }
+    }, [isCreateNewPersonEnabled])
 
     const handleCreateNewPersonCheckbox = (isEnabled: boolean) => {
-        console.log('handleCreateNewPersonCheckbox triggered', )
-
         setIsCreateNewPersonEnabled(!!isEnabled);
     }
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setNewPersonName(value)
+        setNewPerson(prev => ({
+            ...prev,
+            name: value
+        }))
+    }
+
+    // This is needed when a user types in a new person name and hits "Enter". Otherwise will submit whole parent form.
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleCreateNewPerson();
+        } 
+    }
+
+    const handleCreateNewPerson = async () => {
+
+        if (!authState || !authState.user || !authState.user.uid || !newPerson.name.trim()) { return }; // Guard
+
+        setIsCreatingNewPerson(true);
+
+        try {
+            const newDocRef = doc(getPeopleCollRef(authState.user.uid));
+            const newDocData: Person = {
+                ...newPerson,
+                id: newDocRef.id,
+                createdAt: serverTimestamp(),
+                updatedAt: serverTimestamp()
+            }
+
+            await setDoc(newDocRef, newDocData);
+
+            setIsCreateNewPersonEnabled(false);
+            setNewPerson(DEFAULT_PERSON);
+            setIsCreatingNewPerson(false);
+
+        } catch (error) {
+            console.error('Error creating new person. Error:', error);
+            setIsCreatingNewPerson(false);
+        }
     }
 
     return (
@@ -67,14 +115,26 @@ export function FormPeopleSelector({
             )}
 
             {allowCreateNewPerson && isCreateNewPersonEnabled && (
-                <FormInput
-                    type='text'
-                    name='newPersonName'
-                    disabled={disabled}
-                    value={newPersonName}
-                    onChange={handleInputChange}
-
-                />
+                <fieldset className={styles.createNewPersonFieldset}>
+                    <legend>Create New Person</legend>
+                    <FormInput
+                        ref={newPersonInputRef}
+                        type='text'
+                        name='newPersonName'
+                        disabled={isCreatingNewPerson}
+                        value={newPerson.name}
+                        onChange={handleInputChange}
+                        onKeyDown={handleInputKeyDown}
+                    />
+                    <button 
+                        type='button' 
+                        disabled={isCreatingNewPerson}
+                        className={styles.createNewPersonButton}
+                        onClick={handleCreateNewPerson}
+                    >
+                        Create Person
+                    </button>
+                </fieldset>
             )}
         </fieldset>
     )
