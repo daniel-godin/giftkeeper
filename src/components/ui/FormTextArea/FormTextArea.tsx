6d9d91981e9b406b5d7be026535a17b2d6bf55d5
Daniel Godin <DanielEGodin@gmail.com>
2025-07-25 07:38:37 -0700

    v1 complete. Works similarly to the other Form Components

--- src/components/ui/FormTextArea/FormTextArea.tsx
+++ src/components/ui/FormTextArea/FormTextArea.tsx
@@ -1,8 +1,64 @@
 import styles from './FormTextArea.module.css'
 
-export function FormTextArea() {
+interface FormTextAreaProps {
+    label?: string;
+    labelClassName?: string;
 
-    return (
-        <>FormTextArea</>
+    name?: string;
+    id?: string;
+    placeholder?: string;
+    required?: boolean;
+    disabled?: boolean; // For Disabling During Submission
+    value?: string;
+    onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+    onFocus?: () => void;
+    onBlur?: () => void;
+    inputClassName?: string
+
+    // For type: 'textarea':
+    rows?: number;
+}
+
+export function FormTextArea({
+    label,
+    labelClassName,
+    name,
+    id,
+    placeholder,
+    required = false,
+    disabled = false,
+    value,
+    onChange,
+    onFocus,
+    onBlur,
+    inputClassName,
+    rows = 4
+} : FormTextAreaProps) {
+
+    const textAreaElement = (
+        <textarea
+            name={name}
+            id={id}
+            placeholder={placeholder}
+            required={required} // Default: false
+            disabled={disabled} // Default: false
+            value={value}
+            onChange={onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            aria-required={required}
+            className={`${styles.textarea} ${inputClassName || ''}`}
+            rows={rows}
+        />
+    )
+
+    // if (label): Render both <label> AND <textarea>, otherwise render just <textarea>
+    return label ? (
+        <label className={`${styles.label} ${labelClassName || ''}`}>
+            {label} {required && (<span className={styles.required}>*</span>)}
+            {textAreaElement}
+        </label>
+    ) : (
+        textAreaElement
     )
 }
