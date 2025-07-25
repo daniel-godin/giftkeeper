1ce05f32e72b4d6c48a6c1f4619194cf775f52ec
Daniel Godin <DanielEGodin@gmail.com>
2025-07-25 15:37:40 -0700

    Removing unused types/fields. May add some back later. For clarity

--- src/types/PersonType.ts
+++ src/types/PersonType.ts
@@ -1,5 +1,4 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
-import { CalendarDate } from "./CommonTypes";
 
 export interface Person {
     id?: string;
@@ -9,26 +8,8 @@ export interface Person {
     birthday?: string;
 
     // Optional Data
-    avatar?: string; // URL to profile image
-    giftBudgetRange?: { min?: number; max?: number; }; 
     notes?: string;
     relationship?: string;
-
-    // Additional special dates beyond birthday. Anniversary, etc.
-    specialDates?: Array<{
-        date: CalendarDate;
-        title: string;
-        recurring: boolean;
-    }>
-
-    // Gift preferences:
-    preferences?: {
-        likes?: string[];
-        dislikes?: string[];
-        sizes?: Record<string, string>; // Clothing sizes, etc.
-        favoriteColors?: string[];
-        interests?: string[]; // Baseball, Bobbleheads, Nutcrackers, Photography, et al.
-    }
 
     // Metadata
     createdAt?: Timestamp | FieldValue;
