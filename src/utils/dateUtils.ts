f1d8128331b3debd169249ae6c3265e99d8db61c
Daniel Godin <DanielEGodin@gmail.com>
2025-07-27 07:57:40 -0700

    Added two date validation functions to help prevent users from inputing birthdays in the future, or event dates in the past

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -46,3 +46,27 @@ export const getDaysUntilDate = (date: string) => {
     const diffTime = nextBirthday.getTime() - today.getTime();
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
+
+// Checks if birthday Date is today or in the past.
+export const isValidBirthday = (birthday: string): boolean => {
+    if (!birthday) { return false }; // Guard Clause
+
+    const [year, month, day] = birthday.split('-').map(Number); // Convert birthday to Date.UTC compatible input.
+    const birthdayTimestamp = Date.UTC(year, month - 1, day);
+    const now = Date.now();
+
+    return birthdayTimestamp <= now; // If birthdayTimestamp <= now, return true. Birthdays can only be in the past technically.
+}
+
+// Checking whether an eventDate is today or later.
+export const isValidEventDate = (eventDate: string): boolean => {
+    if (!eventDate) { return false }; // Guard Clause
+
+    const [year, month, day] = eventDate.split('-').map(Number); // Convert eventDate to Date.UTC compatible input.
+    const eventTimestamp = Date.UTC(year, month - 1, day);
+
+    const today = new Date();
+    const todayAtMidnightUTC = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()); // Need to convert today to UTC midnight for a proper comparison.
+
+    return eventTimestamp >= todayAtMidnightUTC;
+}
