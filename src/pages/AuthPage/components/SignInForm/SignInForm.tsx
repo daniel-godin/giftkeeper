12e29a875414be0541c890cbeea74dca30cfb0a4
Daniel Godin <DanielEGodin@gmail.com>
2025-07-30 07:13:27 -0700

    Using <FormInput> & <FormSubmitButton> Components, also using <output> instead of a basic <p>

--- src/pages/AuthPage/components/SignInForm/SignInForm.tsx
+++ src/pages/AuthPage/components/SignInForm/SignInForm.tsx
@@ -4,6 +4,7 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../../../firebase/firebase';
 import { useNavigate } from 'react-router';
 import { AuthMode } from '../../AuthPage';
+import { FormInput, FormSubmitButton } from '../../../../components/ui';
 
 interface SignInFormProps {
     authMode: AuthMode;
@@ -20,6 +21,7 @@ export function SignInForm ({ setAuthMode } : SignInFormProps) {
 
     const [status, setStatus] = useState<string>('');
     const [formData, setFormData] = useState<SignInFormData>({email: '', password: ''});
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -33,15 +35,19 @@ export function SignInForm ({ setAuthMode } : SignInFormProps) {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
+        setIsSubmitting(true);
         setStatus('Signing in...');
+
         try {
             await signInWithEmailAndPassword(auth, formData.email, formData.password);
             setStatus('Successfully Signed In. Redirecting You...');
             setTimeout(() => {
                 navigate('/dashboard');
-            }, 1500)
+                setIsSubmitting(false);
+            }, 1000)
         } catch (error) {
             console.error('Error Signing In. Error:', error);
+            setIsSubmitting(false);
             setStatus('Error Signing In');
         }
     }
@@ -49,32 +55,39 @@ export function SignInForm ({ setAuthMode } : SignInFormProps) {
     return (
         <form className={styles.signInForm} onSubmit={handleSubmit}>
             <h2 className={styles.formHeader}>Sign In</h2>
-            <label className={styles.label}>Email:
-                <input
-                    type='email'
-                    id='email'
-                    name='email'
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    className={styles.input}
-                />
-            </label>
-            <label className={styles.label}>Password:
-                <input
-                    type='password'
-                    id='password'
-                    name='password'
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    className={styles.input}
-                />
-            </label>
 
-            <button className={styles.button}>Sign In</button>
+            {/* Sign In Email: */}
+            <FormInput
+                label='Email:'
+                type='email'
+                name='email'
+                required={true}
+                disabled={isSubmitting}
+                value={formData.email}
+                onChange={handleInputChange}
+            />
+
+            {/* Sign In Password: */}
+            <FormInput
+                label='Password:'
+                type='password'
+                name='password'
+                required={true}
+                disabled={isSubmitting}
+                value={formData.password}
+                onChange={handleInputChange}
+            />
+
+            <output>{status}</output>
+
+            <FormSubmitButton
+                text='Sign In'
+                isSubmitting={isSubmitting}
+                submittingText='Signing In...'
+                disabled={!formData.email.trim() || !formData.password.trim()}
+            />
 
             <button type='button' className={styles.forgotPasswordButton} onClick={() => setAuthMode('password_reset')}>Forgot Password?</button>
-
-            {status && (<p>{status}</p>)}
         </form>
     )
 }
