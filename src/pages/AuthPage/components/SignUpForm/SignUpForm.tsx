29c9e5d0a33b3248171c443fcdadff4e25fe145a
Daniel Godin <DanielEGodin@gmail.com>
2025-07-30 07:42:53 -0700

    using reusable <Form> components & added the DEFAULT_USER_PROFILE object for future data

--- src/pages/AuthPage/components/SignUpForm/SignUpForm.tsx
+++ src/pages/AuthPage/components/SignUpForm/SignUpForm.tsx
@@ -4,6 +4,8 @@ import { FormEvent, useState } from 'react';
 import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import { auth, db } from '../../../../firebase/firebase';
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { FormInput, FormSubmitButton } from '../../../../components/ui';
+import { DEFAULT_USER_PROFILE } from '../../../../constants/defaultObjects';
 
 interface SignUpFormData {
     email: string;
@@ -32,11 +34,12 @@ export function SignUpForm () {
         if (!formData.email || !formData.password) { return }; // Guard Clause
         if (formData.password.length < 8) { 
             setStatus('Password must be at least 8+ characters'); 
-            return 
+            return;
         };
 
         setStatus('Signing Up...');
         setIsSubmitting(true);
+
         try {
             const newUser = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
             await sendEmailVerification(newUser.user);
@@ -44,8 +47,8 @@ export function SignUpForm () {
             // Create User Document In Firestore (db, 'users', {userId})
             const newUserDocRef = doc(db, 'users', newUser.user.uid);
             const newUserData = {
+                ...DEFAULT_USER_PROFILE,
                 id: newUser.user.uid,
-                email: newUser.user.email,
 
                 createdAt: serverTimestamp(),
                 updatedAt: serverTimestamp()
@@ -54,9 +57,10 @@ export function SignUpForm () {
             await setDoc(newUserDocRef, newUserData);
 
             setStatus('Check Email To Verify Your Email.  Redirecting you to dashboard...');
+            
             setTimeout(() => {
                 navigate('/dashboard');
-            }, 1500)
+            }, 1000)
         } catch (error: any) {
             console.error('Error Creating Account. Error:', error);
 
@@ -76,31 +80,37 @@ export function SignUpForm () {
     return (
         <form className={styles.signUpForm} onSubmit={handleSubmit}>
             <h2 className={styles.formHeader}>Sign Up</h2>
-            <label className={styles.label}>Email:
-                <input
-                    type='email'
-                    name='email'
-                    value={formData.email}
-                    disabled={isSubmitting}
-                    onChange={handleInputChange}
-                    className={styles.input}
-                />
-            </label>
-            <label className={styles.label}>Password:
-                <input
-                    type='password'
-                    name='password'
-                    minLength={8}
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    disabled={isSubmitting}
-                    className={styles.input}
-                />
-            </label>
 
-            <button type='submit' className={styles.button} disabled={isSubmitting}>Sign Up</button>
+            {/* Sign Up Email: */}
+            <FormInput
+                label='Email:'
+                type='email'
+                name='email'
+                required={true}
+                disabled={isSubmitting}
+                value={formData.email}
+                onChange={handleInputChange}
+            />
 
-            {status && (<p className={styles.statusText}>{status}</p>)}
+            {/* Sign Up Password: (note: no confirming password input. Use email verify.) */}
+            <FormInput
+                label='Password:'
+                type='password'
+                name='password'
+                required={true}
+                disabled={isSubmitting}
+                value={formData.password}
+                onChange={handleInputChange}
+            />
+
+            <output>{status}</output>
+
+            <FormSubmitButton
+                text='Sign Up'
+                isSubmitting={isSubmitting}
+                submittingText='Signing Up...'
+                disabled={!formData.email.trim() || !formData.password.trim()}
+            />
         </form>
     )
 }
