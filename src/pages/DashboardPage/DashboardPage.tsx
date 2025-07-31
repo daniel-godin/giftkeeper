3ba41dcf69c09c66be08bbb09aa30d108cf3716e
Daniel Godin <DanielEGodin@gmail.com>
2025-07-31 06:54:17 -0700

    Moved DashboardHeader into their own file

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import styles from './DashboardPage.module.css'
 import { Dashboard } from './components/Dashboard/Dashboard';
+import { DashboardHeader } from './components/DashboardHeader/DashboardHeader';
 
 export function DashboardPage () {
 
@@ -13,31 +14,6 @@ export function DashboardPage () {
                 <Dashboard />
             </div>  */}
         </section>
-    )
-}
-
-function DashboardHeader () {
-
-    return (
-        <header className={styles.dashboardHeader}>
-            <h2 className={styles.pageTitle}>Dashboard</h2>
-                
-            <div className={styles.createButtonsContainer}>
-                <button className={styles.createButton}>Add Gift</button>
-                <button className={styles.createButton}>Create Event</button>
-                <button className={styles.createButton}>Add Person</button>
-            </div>
-
-            <div className={styles.alertsContainer}>
-                Christmas is in 5 days and you need to purchase gifts for 4 people. Time to act!
-            </div>
-
-            <div className={styles.overviewStatsContainer}>
-                <button className={styles.statButton}>Events</button>
-                <button className={styles.statButton}>Gifts</button>
-                <button className={styles.statButton}>People</button>
-            </div>
-        </header>
     )
 }
 
--- src/pages/DashboardPage/components/DashboardHeader/DashboardHeader.tsx
+++ src/pages/DashboardPage/components/DashboardHeader/DashboardHeader.tsx
@@ -3,6 +3,24 @@ import styles from './DashboardHeader.module.css'
 export function DashboardHeader () {
 
     return (
-        <>DashboardHeader</>
+        <header className={styles.dashboardHeader}>
+            <h2 className={styles.pageTitle}>Dashboard</h2>
+                
+            <div className={styles.createButtonsContainer}>
+                <button className={styles.createButton}>Add Gift</button>
+                <button className={styles.createButton}>Create Event</button>
+                <button className={styles.createButton}>Add Person</button>
+            </div>
+
+            <div className={styles.alertsContainer}>
+                Christmas is in 5 days and you need to purchase gifts for 4 people. Time to act!
+            </div>
+
+            <div className={styles.overviewStatsContainer}>
+                <button className={styles.statButton}>Events</button>
+                <button className={styles.statButton}>Gifts</button>
+                <button className={styles.statButton}>People</button>
+            </div>
+        </header>
     )
 }
