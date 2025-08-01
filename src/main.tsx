7ad6f7940934b9f50107e309980cbcee81db3b32
Daniel Godin <DanielEGodin@gmail.com>
2025-08-01 16:09:51 -0700

    Removed TS ! assertion on root. Added better error handling

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import './index.css'
 import { App } from './App'
 import { AuthProvider } from './contexts/AuthContext'
 
-
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) { throw new Error('Root element not found'); };
+createRoot(rootElement).render(
     <StrictMode>
         <AuthProvider>
             <App />
