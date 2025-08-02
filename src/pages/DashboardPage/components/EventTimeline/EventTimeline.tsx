6205e276cb2933fa9b3aa9d89db5cbd7e9ab756b
Daniel Godin <DanielEGodin@gmail.com>
2025-08-02 12:13:22 -0700

    Added funtionality to timeline control buttons

--- src/pages/DashboardPage/components/EventTimeline/EventTimeline.tsx
+++ src/pages/DashboardPage/components/EventTimeline/EventTimeline.tsx
@@ -1,14 +1,11 @@
-import { useEvents } from '../../../../contexts/EventsContext'
-import { Event } from '../../../../types/EventType';
+import { useState } from 'react';
 import { EventCard } from './components/EventCard/EventCard';
 import styles from './EventTimeline.module.css'
-
-interface EventTimelineProps {
-
-}
+import { useUpcomingEvents } from '../../../../hooks/useUpcomingEvents';
 
 export function EventTimeline() {
-    const { events } = useEvents();
+    const [selectedPeriod, setSelectedPeriod] = useState<'30days' | '90days' | 'thisyear'>('thisyear');
+    const events = useUpcomingEvents(undefined, selectedPeriod);
 
     return (
         <div className={styles.eventTimeline}>
@@ -16,16 +13,22 @@ export function EventTimeline() {
                 <h3 className={styles.eventTimelineTitle}>Event Timeline</h3>
 
                 <div className={styles.eventTimelineControls}>
-                    <button className={`unstyled-button ${styles.eventTimelineControlButton} ${styles.selected}`}>30 Days</button>
-                    <button className={`unstyled-button ${styles.eventTimelineControlButton}`}>90 Days</button>
-                    <button className={`unstyled-button ${styles.eventTimelineControlButton}`}>This Year</button>
-
+                    <button 
+                        className={`unstyled-button ${styles.eventTimelineControlButton} ${selectedPeriod === '30days' ? styles.selected : ''}`}
+                        onClick={() => setSelectedPeriod('30days')}
+                    >30 Days</button>
+                    <button 
+                        className={`unstyled-button ${styles.eventTimelineControlButton} ${selectedPeriod === '90days' ? styles.selected : ''}`}
+                        onClick={() => setSelectedPeriod('90days')}
+                    >90 Days</button>
+                    <button 
+                        className={`unstyled-button ${styles.eventTimelineControlButton} ${selectedPeriod === 'thisyear' ? styles.selected : ''}`}
+                        onClick={() => setSelectedPeriod('thisyear')}
+                    >This Year</button>
                 </div>                
-                
             </header>
 
             <div className={styles.eventTimelineCardsContainer}>
-                {/* events.map... use re-usable component. */}
                 {events.map(event => (
                     <EventCard key={event.id} data={event} />
                 ))}
