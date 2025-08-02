4c750454f8a8e332ba1bc97f46e8fd906bf8c552
Daniel Godin <DanielEGodin@gmail.com>
2025-08-02 08:05:25 -0700

    cleanup

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,4 @@
 import styles from './DashboardPage.module.css'
-// import { Dashboard } from './components/Dashboard/Dashboard';
 import { DashboardHeader } from './components/DashboardHeader/DashboardHeader';
 import { EventTimeline } from './components/EventTimeline/EventTimeline';
 
