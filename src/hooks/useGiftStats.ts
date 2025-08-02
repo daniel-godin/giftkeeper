40402366e86f7e28872acb15f68d2ab95aaa6112
Daniel Godin <DanielEGodin@gmail.com>
2025-08-02 09:58:57 -0700

    Created a useGiftStats hook to save stats that will be used throughout the app. Filtering by personId & eventId coming later

---
+++ src/hooks/useGiftStats.ts
@@ -0,0 +1,40 @@
+/**
+ * Returns data/statistics about gift items (db, 'users', {userId}, 'giftItems') collection.
+ * Useful for dashboard stats and everywhere else in the app
+ */
+
+import { useMemo } from "react";
+import { useGiftItems } from "../contexts/GiftItemsContext";
+
+interface GiftItemsStats {
+    total: number;
+    ideas: number;
+    purchased: number;
+    // Open for more later.
+}
+
+export function useGiftStats() : GiftItemsStats {
+    const { giftItems } = useGiftItems();
+
+    // TODO:  Add filtering options later.
+    // Filter By:  personId or eventId for PersonPage/EventPage.
+
+    return useMemo(() => {
+
+        const stats: GiftItemsStats = {
+            total: giftItems.length,
+            ideas: giftItems.filter(item => item.status === 'idea').length,
+            purchased: giftItems.filter(item => item.status === 'purchased').length,
+        }
+
+        return stats
+    }, [giftItems])
+
+
+
+
+
+
+
+
+}
