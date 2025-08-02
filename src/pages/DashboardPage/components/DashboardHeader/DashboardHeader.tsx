f816da9a3b528966ad9563b8ece0bde9dea61461
Daniel Godin <DanielEGodin@gmail.com>
2025-08-02 09:11:20 -0700

    Gift Stats work, but it is clunky. Needs real-time listener

--- src/pages/DashboardPage/components/DashboardHeader/DashboardHeader.tsx
+++ src/pages/DashboardPage/components/DashboardHeader/DashboardHeader.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './DashboardHeader.module.css'
 import { QuickActionButton } from './components/QuickActionButton/QuickActionButton'
 import { CalendarFold, Gift, UsersRound } from 'lucide-react'
@@ -10,12 +10,19 @@ import { useEvents } from '../../../../contexts/EventsContext';
 import { usePeople } from '../../../../contexts/PeopleContext';
 import { useUpcomingEvents } from '../../../../hooks/useUpcomingEvents';
 import { usePeopleWithBirthdays } from '../../../../hooks/usePeopleWithBirthdays';
+import { getCountFromServer, query, where } from 'firebase/firestore';
+import { getGiftItemsCollRef } from '../../../../firebase/firestore';
+import { useAuth } from '../../../../contexts/AuthContext';
 
 export function DashboardHeader () {
+    const { authState } = useAuth();
     const { events } = useEvents();
     const { people } = usePeople();
     const upcomingEvents = useUpcomingEvents();
     const peopleWithBirthdays = usePeopleWithBirthdays();
+
+    // Stats States
+    const [giftItemsStats, setGiftItemsStats] = useState({ total: 0, ideas: 0, purchased: 0 });
 
     // Modal States (boolean) -- Triggered By QuickActionButtons
     const [isAddGiftItemModalOpen, setIsAddGiftItemModalOpen] = useState<boolean>(false);
@@ -24,6 +31,32 @@ export function DashboardHeader () {
 
     // TODO: Create a useMemo to save all the stats for the overviewStats section.
     // POSSIBLY:  Move this to parent component, might need these same stats in EventTimeline
+
+    useEffect(() => {
+        const getGiftItemsStats = async () => {
+            if (!authState.user) { return }; // Guard/Optimization Clause
+
+            const totalQuery = query(getGiftItemsCollRef(authState.user.uid));
+            const ideasQuery = query(getGiftItemsCollRef(authState.user.uid), where('status', '==', 'idea'));
+            const purchasedQuery = query(getGiftItemsCollRef(authState.user.uid), where('status', '==', 'purchased'));
+
+            const total = await getCountFromServer(totalQuery);
+            const ideas = await getCountFromServer(ideasQuery);
+            const purchased = await getCountFromServer(purchasedQuery);
+
+            // console.log(total.data().count, ideas, purchased);
+
+            setGiftItemsStats(prev => ({
+                ...prev,
+                total: total.data().count,
+                ideas: ideas.data().count,
+                purchased: purchased.data().count
+            }))
+
+        }
+
+        getGiftItemsStats()
+    }, []);
 
 
 
@@ -70,10 +103,10 @@ export function DashboardHeader () {
                 <StatCard
                     to='/giftItems'
                     title='GIFTS'
-                    totalCount={30}
+                    totalCount={giftItemsStats.total}
                     breakdownStats={[
-                        {count: 23, label: 'ideas'},
-                        {count: 7, label: 'bought'}
+                        {count: giftItemsStats.ideas, label: 'ideas'},
+                        {count: giftItemsStats.purchased, label: 'bought'}
                     ]}
                 />
 
