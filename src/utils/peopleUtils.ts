e7eb354ec0e0b25a7d0595a49a52b140d7b23f4e
Daniel Godin <DanielEGodin@gmail.com>
2025-08-07 15:13:06 -0700

    No need for peopleUtils.ts anymore. The only function was actually a hook and causing issues.

--- src/utils/peopleUtils.ts
+++
@@ -1,9 +0,0 @@
-// Utilities around people/person.
-import { usePeople } from "../contexts/PeopleContext";
-
-// Returns a person's nickname, or name or 'Unknown'
-export const getPersonNameFromId = (personId: string): string => {
-    const { people } = usePeople();
-    const person = people.find(p => p.id === personId);
-    return person?.nickname || person?.name || 'Unknown';
-}
