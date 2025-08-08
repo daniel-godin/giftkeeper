2f8b5276d85f188e2b0fa3ce394d30f633ec2816
Daniel Godin <DanielEGodin@gmail.com>
2025-08-08 07:46:20 -0700

    formatCurrency tests set up

--- src/utils/currencyUtils.test.ts
+++ src/utils/currencyUtils.test.ts
@@ -1,4 +1,4 @@
-import { centsToDollars } from "./currencyUtils"
+import { centsToDollars, formatCurrency } from "./currencyUtils"
 import { test, expect } from 'vitest'
 
 test('Convert cents to dollars - basic cases', () => {
@@ -16,4 +16,19 @@ test('Convert cents to dollars - edge cases', () => {
 test('Convert cents to dollars - potential problem cases', () => {
     expect(centsToDollars(-100)).toBe(-1) // Negative money?
     expect(centsToDollars(12345)).toBe(123.45) // Odd amounts
+})
+
+// Tests For formatCurrency Function
+test('Convert cents from number to dollar string', () => {
+    expect(formatCurrency(100000)).toBe('$1,000.00');
+    expect(formatCurrency(10000)).toBe('$100.00');
+    expect(formatCurrency(100)).toBe('$1.00');
+    expect(formatCurrency(0)).toBe('$0.00');
+
+    expect(formatCurrency(100125)).toBe('$1,001.25');
+    expect(formatCurrency(125)).toBe('$1.25');
+
+    expect(formatCurrency(-125)).toBe('-$1.25');
+
+    expect(formatCurrency(999999999)).toBe('$9,999,999.99');
 })
