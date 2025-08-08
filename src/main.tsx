ea50d360214a50ff4d812750ab165710bb82060e
Daniel Godin <DanielEGodin@gmail.com>
2025-08-08 14:52:21 -0700

    Added ToastProvider for use throughout the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { App } from './App'
 import { AuthProvider } from './contexts/AuthContext'
 import * as Sentry from "@sentry/react"
+import { ToastProvider } from './contexts/ToastContext'
 
 Sentry.init({
     dsn: "https://b3386d5a4a301b84469386a7b01be48c@o4509810252906496.ingest.us.sentry.io/4509810256510976",
@@ -17,7 +18,9 @@ if (!rootElement) { throw new Error('Root element not found'); };
 createRoot(rootElement).render(
     <StrictMode>
         <AuthProvider>
-            <App />
+            <ToastProvider>
+                <App />
+            </ToastProvider>
         </AuthProvider>
     </StrictMode>,
 )
