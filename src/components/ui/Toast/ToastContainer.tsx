c2a8108e96e6d5a9a53b68400ee6c6c21ad60754
Daniel Godin <DanielEGodin@gmail.com>
2025-08-09 07:26:29 -0700

    ToastContainer how displays in approximately the correct place for both mobile and desktop.  Also gets it's data from the ToastContext

--- src/components/ui/Toast/ToastContainer.tsx
+++ src/components/ui/Toast/ToastContainer.tsx
@@ -1,8 +1,22 @@
+import { createPortal } from 'react-dom';
+import { useToast } from '../../../contexts/ToastContext'
 import styles from './ToastContainer.module.css'
 
-export function ToastContainer() {
+interface ToastContainerProps {
 
-    return (
-        <>ToastContainer</>
+}
+
+export function ToastContainer() {
+    const { toasts, addToast, removeToast } = useToast();
+
+    return createPortal (
+        <div className={styles.toastContainer}>
+            {toasts.map(toast => (
+                <div key={toast.id} className={styles.toast}>
+                    <p>{toast.message}</p>
+                </div>
+            ))}
+        </div>,
+        document.getElementById('toast-root') || document.body
     )
 }
