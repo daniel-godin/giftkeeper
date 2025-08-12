e5030a61c5f66d5dd022d3399da8fcb0308df7ca
Daniel Godin <DanielEGodin@gmail.com>
2025-08-12 14:10:22 -0700

    Removed unused functions

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -5,15 +5,3 @@ export const capitalizeFirst = (str: string): string => {
     if (!str) { return str }; // Guard Clause -- Returns Incoming String.
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
-
-// Capitalizes First Letter of Each Word
-export const capitalizeWords = (str: string): string => {
-    if (!str) { return str }; // Guard Clause -- Returns Incoming String.
-    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
-}
-
-// Removes Excess & Lowercase Everything
-export const sanitizeStringAllLowerCase = (str: string): string => {
-    if (!str) { return str }; // Guard Clause -- Returns Incoming String.
-    return str.trim().toLowerCase();
-}
